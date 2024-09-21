fb482888c3baf4068fe3ec429dc9b558b672d393
olha-tryhub-teacher <olha.tryhub.work@gmail.com>
2024-09-21 14:45:25 +0300

    Update 3.py

--- pythonStart2/21-09-2024/examples/3.py
+++ pythonStart2/21-09-2024/examples/3.py
@@ -1,15 +1,30 @@
 class Widget():
     #властивості класа (в конструкторі)
-    ...
+    def __init__(self, text, x, y):
+        self.text = text
+        self.x = x
+        self.y = y
     #методи
+    def print_info(self):
+        print("Напис: ", self.text)
+        print("Розташування: ", self.x, self.y)
 
 class Button(Widget):
     #доповнені властивості класа (в конструкторі)
-    ...
+    def __init__(self, text, x, y, is_clicked):
+        super().__init__(text, x, y)
+        self.is_clicked = is_clicked
     #нові методи
-
+    def click(self):
+        self.is_clicked = True
+        print("Ви зареєстровані")
 
 #створи екземпляр класа Button
 #якщо на питання «Хочете зареєструватися?» користувач відповів «так», то застосуйте метод click
+btn = Button("Реєстрація", 100, 100, False)
 
-
+answer = input("Хочете зареєструватися? (так / ні):").capitalize()
+if answer == "Так":
+    btn.click()
+else:
+    print("А шкода!")
