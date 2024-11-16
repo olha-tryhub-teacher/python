c2a6e310a3176606527b3b8017379cac2bf3dfa6
olha-tryhub-teacher <olha.tryhub.work@gmail.com>
2024-11-16 13:46:37 +0200

    Update quotes_prog.py

--- pythonStart2/module3/lesson2/quotes_prog.py
+++ pythonStart2/module3/lesson2/quotes_prog.py
@@ -1,1 +1,14 @@
-#напиши тут свою програму
+# 1
+with open("quotes.txt", "r", encoding="utf-8") as file:
+    data = file.read()
+print(data)
+# 2
+author = input("Хто написав цей вірш?")
+# 3
+with open("quotes.txt", "a", encoding="utf-8") as file:
+    file.write("\n(" + author + ")")
+
+
+with open("quotes.txt", "r", encoding="utf-8") as file:
+    data = file.read()
+print(data)
