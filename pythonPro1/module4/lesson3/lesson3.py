42a30091836cced99c510abf360c1bf5b1b048a3
olha-tryhub-teacher <olha.tryhub.work@gmail.com>
2024-11-30 13:17:51 +0200

    Update lesson3.py

--- pythonPro1/module4/lesson3/lesson3.py
+++ pythonPro1/module4/lesson3/lesson3.py
@@ -7,6 +7,15 @@ df2["is_employed"] = df2["career_end"] > df2["career_start"]
 df2.drop(["id", "last_seen", "langs", "bdate",
           "career_end", "career_start"], axis=1, inplace=True)
 
+def remove_False_life_main(row):
+    if row["life_main"] not in "12345678910":
+        return -1
+    return int(row["life_main"])
+def remove_False_people_main(row):
+    if row["people_main"] not in "12345678910":
+        return -1
+    return int(row["people_main"])
+
 df2["life_main"] = df2.apply(remove_False_life_main, axis=1)
 df2["people_main"] = df2.apply(remove_False_people_main, axis=1)
 
@@ -22,3 +31,5 @@ result = pd.DataFrame({'id': ID, 'result': y_pred2})
 
 # зберігаємо в окреми CSV файл наш  результат із лише двома стовпчиками
 result.to_csv("result.csv", index=False)
+
+
