b217a3ef63798541feb3235a0fdb98e06abad522
olha-tryhub-teacher <olha.tryhub.work@gmail.com>
2024-12-12 19:09:20 +0200

    Update illia.py

--- illia.py
+++ illia.py
@@ -1,11 +1,11 @@
-    def del_note(self):
+    def add_tag(self):
         if self.ui.listWidget.currentItem():
             key = self.ui.listWidget.currentItem().text()
-            del self.notes[key]
-            self.ui.listWidget.clear()
-            self.ui.listWidget_2.clear()
-            self.ui.textEdit.clear()
-            self.ui.listWidget.addItems(self.notes)
-            self.save_to_file()
+            tag = self.ui.lineEdit.text()
+            if tag and tag not in self.notes[key]["теги"]:
+                self.notes[key]["теги"].append(tag)
+                self.ui.listWidget_2.addItem(tag)
+                self.ui.lineEdit.clear()
+                self.save_to_file()
         else:
-            QtWidgets.QMessageBox.warning(self, "Помилка", "Замітка для видалення не обрана!")
+            QtWidgets.QMessageBox.warning(self, "Помилка", "Замітка для додавання тега не обрана!")
