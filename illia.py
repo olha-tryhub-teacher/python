4479fa69120aa34c02979aba780ec42919c83cb6
olha-tryhub-teacher <olha.tryhub.work@gmail.com>
2024-12-12 19:13:50 +0200

    Update illia.py

--- illia.py
+++ illia.py
@@ -1,11 +1,12 @@
-    def del_tag(self):
-        if self.ui.listWidget_2.currentItem():
-            key = self.ui.listWidget.currentItem().text()
-            tag = self.ui.listWidget_2.currentItem().text()
-            self.notes[key]["теги"].remove(tag)
-            self.ui.listWidget_2.clear()
-            self.ui.listWidget_2.addItems(self.notes[key]["теги"])
-            self.save_to_file()
-        else:
-            QtWidgets.QMessageBox.warning(
-                self, "Помилка", "Тег для видалення не обраний!")
+    def search_tag(self):
+        tag = self.ui.lineEdit.text()
+        if self.ui.pushButton_7.text() == "Шукати замітки по тегу" and tag:
+            notes_filtered = {k: v for k, v in self.notes.items() if tag in v["теги"]}
+            self.ui.listWidget.clear()
+            self.ui.listWidget.addItems(notes_filtered)
+            self.ui.pushButton_7.setText("Скинути пошук")
+        elif self.ui.pushButton_7.text() == "Скинути пошук":
+            self.ui.listWidget.clear()
+            self.ui.listWidget.addItems(self.notes)
+            self.ui.lineEdit.clear()
+            self.ui.pushButton_7.setText("Шукати замітки по тегу")
