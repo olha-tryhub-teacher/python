894a2011a014178c2f9bc3df34feb222c5b7c6bb
olha-tryhub-teacher <olha.tryhub.work@gmail.com>
2024-12-12 19:02:30 +0200

    Update illia.py

--- illia.py
+++ illia.py
@@ -1,7 +1,5 @@
-    def add_note(self):
-        note_name, ok = QtWidgets.QInputDialog.getText(
-            self, "Додати замітку", "Назва замітки: ")
-        if ok and note_name != "":
-            self.notes[note_name] = {"текст": "", "теги": []}
-            self.ui.listWidget.addItem(note_name)
-            self.save_to_file()
+    def show_note(self):
+        key = self.ui.listWidget.currentItem().text()
+        self.ui.textEdit.setText(self.notes[key]["текст"])
+        self.ui.listWidget_2.clear()
+        self.ui.listWidget_2.addItems(self.notes[key]["теги"])
