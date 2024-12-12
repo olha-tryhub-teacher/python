544135979e2596f2e5c9b6ac82c2fe48734c2560
olha-tryhub-teacher <olha.tryhub.work@gmail.com>
2024-12-12 19:04:43 +0200

    Update illia.py

--- illia.py
+++ illia.py
@@ -1,5 +1,7 @@
-    def show_note(self):
-        key = self.ui.listWidget.currentItem().text()
-        self.ui.textEdit.setText(self.notes[key]["текст"])
-        self.ui.listWidget_2.clear()
-        self.ui.listWidget_2.addItems(self.notes[key]["теги"])
+    def save_note(self):
+        if self.ui.listWidget.currentItem():
+            key = self.ui.listWidget.currentItem().text()
+            self.notes[key]["текст"] = self.ui.textEdit.toPlainText()
+            self.save_to_file()
+        else:
+            QtWidgets.QMessageBox.warning(self, "Помилка", "Замітка для збереження не вибрана!")
