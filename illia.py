6899edf6b45986586e22a387df8c88bdb7c07ee9
olha-tryhub-teacher <olha.tryhub.work@gmail.com>
2024-12-12 19:15:09 +0200

    Update illia.py

--- illia.py
+++ illia.py
@@ -1,12 +1,5 @@
-    def search_tag(self):
-        tag = self.ui.lineEdit.text()
-        if self.ui.pushButton_7.text() == "Шукати замітки по тегу" and tag:
-            notes_filtered = {k: v for k, v in self.notes.items() if tag in v["теги"]}
-            self.ui.listWidget.clear()
-            self.ui.listWidget.addItems(notes_filtered)
-            self.ui.pushButton_7.setText("Скинути пошук")
-        elif self.ui.pushButton_7.text() == "Скинути пошук":
-            self.ui.listWidget.clear()
-            self.ui.listWidget.addItems(self.notes)
-            self.ui.lineEdit.clear()
-            self.ui.pushButton_7.setText("Шукати замітки по тегу")
+import sys
+app = QtWidgets.QApplication(sys.argv)
+mainWindow = NotesWindow()
+mainWindow.show()
+sys.exit(app.exec_())
