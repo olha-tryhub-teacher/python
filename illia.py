a84a2036d89dbc650f98f3c820731e739eca76eb
olha-tryhub-teacher <olha.tryhub.work@gmail.com>
2024-12-12 18:56:36 +0200

    Update illia.py

--- illia.py
+++ illia.py
@@ -1,1 +1,5 @@
 
+    def save_to_file(self):
+        with open("notes_data.json", "w", encoding="utf-8") as file:
+            json.dump(self.notes, file, sort_keys=True,
+                      ensure_ascii=False)
