d14cd0793d1cf6e72cc565cc85f6a5ac7018a405
olha-tryhub-teacher <olha.tryhub.work@gmail.com>
2025-03-01 13:15:42 +0200

    Update code.py

--- code.py
+++ code.py
@@ -1,150 +1,41 @@
-import sqlite3
+from random import randint
+from flask import Flask, session, redirect, url_for
+from quiz_db import get_question
 
-db_name = "quiz.db"
-conn = None
-curor = None
-
-
-def open():
-    global conn, cursor
-    conn = sqlite3.connect(db_name)
-    cursor = conn.cursor()
+quiz = 0
+last_question = 0
 
 
-def close():
-    cursor.close()
-    conn.close()
+def index():
+    global quiz, last_question
+    max_quiz = 3
+    quiz = randint(1, max_quiz)
+
+    last_question = 0
+    return "<a href='/test'>Тест</a>"
 
 
-def do(query):
-    cursor.execute(query)
-    conn.commit()
+def test():
+    global last_question
+    result = get_question(last_question, quiz)
+    if result is None or len(result) == 0:
+        return redirect(url_for("result"))
+
+    else:
+        last_question = result[0]
+        return "<h1>" + str(quiz) + "<br>" + str(result) + "</h1>"
 
 
-def clear_db():
-    ''' видаляє всі таблиці '''
-    open()
-    query = '''DROP TABLE IF EXISTS quiz_content'''
-    do(query)
-    query = '''DROP TABLE IF EXISTS question'''
-    do(query)
-    query = '''DROP TABLE IF EXISTS quiz'''
-    do(query)
-    close()
-
-def show(table):
-    query = 'SELECT * FROM ' + table
-    open()
-    cursor.execute(query)
-    print(cursor.fetchall())
-    close()
+def result():
+    return "that's all folks!"
 
 
-def show_tables():
-    show('question')
-    show('quiz')
-    show('quiz_content')
-
-
-# task1 Створення таблиць та опис зв'язків між ними
-def create():
-    open()
-    cursor.execute('''PRAGMA foreign_keys=on''')
-
-    do('''CREATE TABLE IF NOT EXISTS quiz (
-           id INTEGER PRIMARY KEY,
-           name VARCHAR)''')
-
-    do('''CREATE TABLE IF NOT EXISTS question (
-               id INTEGER PRIMARY KEY,
-               question VARCHAR,
-               answer VARCHAR,
-               wrong1 VARCHAR,
-               wrong2 VARCHAR,
-               wrong3 VARCHAR)''')
-
-    do('''CREATE TABLE IF NOT EXISTS quiz_content (
-               id INTEGER PRIMARY KEY,
-               quiz_id INTEGER,
-               question_id INTEGER,
-               FOREIGN KEY (quiz_id) REFERENCES quiz (id),
-               FOREIGN KEY (question_id) REFERENCES question (id) )''')
-    close()
-
-
-# task 2: Заповнення таблиць даними
-def add_questions():
-    questions = [
-        ('Скільки місяців на рік мають 28 днів?', 'Всі', 'Один', 'Жодного', 'Два'),
-        ('Яким стане зелена скеля, якщо впаде в Червоне море?', 'Мокрим', 'Червоним', 'Не зміниться', 'Фіолетовим'),
-        ('Якою рукою краще розмішувати чай?', 'Ложкою', 'Правою', 'Лівою', 'Любою'),
-        ('Що не має довжини, глибини, ширини, висоти, а можна виміряти?', 'Час', 'Дурність', 'Море', 'Повітря'),
-        ('Коли сіткою можна витягнути воду?', 'Коли вода замерзла', 'Коли немає риби', 'Коли спливла золота рибка',
-         'Коли сітка порвалася'),
-        ('Що більше слона і нічого не важить?', 'Тінь слона', 'Повітряна куля', 'Парашут', 'Хмара')
-    ]
-    open()
-    cursor.executemany('''INSERT INTO question (question, answer, wrong1, wrong2, wrong3) VALUES (?,?,?,?,?)''',
-                       questions)
-    conn.commit()
-    close()
-
-
-def add_quiz():
-    quizes = [
-        ('Кахут',),
-        ('Хто хоче стати мільйонером?',),
-        ('Найрозумніший',),
-        ("Хто зверху?",)
-    ]
-    open()
-    cursor.executemany('''INSERT INTO quiz (name) VALUES (?)''', quizes)
-    conn.commit()
-    close()
-
-
-def add_links():
-    open()
-    cursor.execute('''PRAGMA foreign_keys=on''')
-    query = "INSERT INTO quiz_content (quiz_id, question_id) VALUES (?,?)"
-    answer = input("Додати зв'язок (y / n)?")
-    while answer != 'n':
-        quiz_id = int(input("id вікторини: "))
-        question_id = int(input("id питання: "))
-        cursor.execute(query, [quiz_id, question_id])
-        conn.commit()
-        answer = input("Додати зв'язок (y / n)?")
-    close()
-
-
-# task 3: Отримання тексту запитання та варіантів відповідей
-def get_question(question_id=0, quiz_id=1):
-    ''' повертає наступне питання після запитання з переданим id
-     для першого запитання передається значення за замовчуванням '''
-    open()
-    query = '''
-    SELECT quiz_content.id, question.question, question.answer, question.wrong1, question.wrong2, question.wrong3
-    FROM question, quiz_content
-    WHERE quiz_content.question_id == question.id
-    AND quiz_content.id => ? AND quiz_content.quiz_id => ?
-    ORDER BY quiz_content.id '''
-    cursor.execute(query, [question_id, quiz_id])
-    result = cursor.fetchone()
-    close()
-    return result
-
-
-def main():
-    clear_db()
-    create()
-    add_questions()
-    add_quiz()
-    add_links()
-    show_tables()
-    # Виведення в консоль питання з id=3, id вікторини = 1
-    print(get_question(1, 3))
-
+# Створюємо об'єкт веб-застосунку:
+app = Flask(__name__)
+app.add_url_rule("/", "index", index)  # створює правило для URL "/"
+app.add_url_rule("/test", "test", test)  # створює правило для URL "/test"
+app.add_url_rule("/result", "result", result)  # створює правило для URL "/result"
 
 if __name__ == "__main__":
-    main()
-
+    # Запускаємо веб-сервер:
+    app.run()
