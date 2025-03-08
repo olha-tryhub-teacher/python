6ee0fcb93552756e99a083e3f9a0ed86ccad0d17
olha-tryhub-teacher <olha.tryhub.work@gmail.com>
2025-03-08 16:10:29 +0200

    Update code.py

--- code.py
+++ code.py
@@ -1,38 +1,26 @@
 from socket import *
 import threading
-import time
 
-def connect():
-    while True:
-        try:
-            sock = socket(AF_INET, SOCK_STREAM)
-            sock.connect(('localhost', 8080))
-            name = input("Введіть ім'я: ")
-            sock.send(name.encode())
-            return sock
-        except:
-            print("З'єднання невдале, пробуємо ще...")
-            time.sleep(1)
+client_socket = socket(AF_INET, SOCK_STREAM)
+name = input("Введіть ім'я: ")
+client_socket.connect(("localhost", 12345))
+client_socket.send(name.encode())
 
-client_socket = connect()
 
 def send_message():
     while True:
-        msg = input()
-        if msg.lower() == 'exit':
+        client_message = input()
+        if client_message.lower() == "exit":
             client_socket.close()
             break
-        client_socket.send(msg.encode())
+        client_socket.send(client_message.encode())
 
-threading.Thread(target=send_message, daemon=True).start()
 
+threading.Thread(target=send_message).start()  # запуск потока відправки повідомлень
 while True:
     try:
         message = client_socket.recv(1024).decode().strip()
         if message:
             print(message)
-        else:
-            break
     except:
-        print("Відключено від чату")
         break
