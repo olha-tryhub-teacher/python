eef131ebd495299eada91f16de5165614a7aee1e
olha-tryhub-teacher <olha.tryhub.work@gmail.com>
2025-03-21 19:09:53 +0200

    Update code.py

--- code.py
+++ code.py
@@ -1,26 +1,13 @@
-from socket import *
-import threading
+class Area():
+    def __init__(self, x=0, y=0, width=10, height=10, color=None):
+        self.rect = Rect(x, y, width, height)  # прямокутник
+        self.fill_color = color
 
-client_socket = socket(AF_INET, SOCK_STREAM)
-name = input("Введіть ім'я: ")
-client_socket.connect(("localhost", 12345))
-client_socket.send(name.encode())
+    def color(self, new_color):
+        self.fill_color = new_color
 
+    def fill(self):
+        draw.rect(mw, self.fill_color, self.rect)
 
-def send_message():
-    while True:
-        client_message = input()
-        if client_message.lower() == "exit":
-            client_socket.close()
-            break
-        client_socket.send(client_message.encode())
-
-
-threading.Thread(target=send_message).start()  # запуск потока відправки повідомлень
-while True:
-    try:
-        message = client_socket.recv(1024).decode().strip()
-        if message:
-            print(message)
-    except:
-        break
+    def outline(self, frame_color, thickness):  # обведення існуючого прямокутника
+        draw.rect(mw, frame_color, self.rect, thickness)
