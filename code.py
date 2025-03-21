fddda6c1c542cfde454a5556a4fda530bae9aad4
olha-tryhub-teacher <olha.tryhub.work@gmail.com>
2025-03-21 19:17:57 +0200

    Update code.py

--- code.py
+++ code.py
@@ -1,8 +1,22 @@
-class Label(Area):
-    def set_text(self, text, fsize=12, text_color=(0, 0, 0)):
-        self.image = font.SysFont('verdana', fsize).render(text, True, text_color)
+YELLOW = (255, 255, 0)
+DARK_BLUE = (0, 0, 100)
+BLUE = (80, 80, 255)
 
-    def draw(self, shift_x=0, shift_y=0):
-        self.fill()
-        self.outline(BLUE, 10)  # Додаємо малювання контуру
-        mw.blit(self.image, (self.rect.x + shift_x, self.rect.y + shift_y))
+cards = []
+num_cards = 4
+
+x = 70
+
+for i in range(num_cards):
+    new_card = Label(x, 170, 70, 100, YELLOW)
+    new_card.outline(BLUE, 10)
+    new_card.set_text('CLICK', 14)
+    cards.append(new_card)
+    x = x + 100
+
+while True:
+    for card in cards:
+        card.draw(10, 30)
+
+    display.update()
+    clock.tick(40)
