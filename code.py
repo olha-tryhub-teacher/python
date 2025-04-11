1cf26a66e3f286afe2f8f594f0df603c3dca6d17
olha-tryhub-teacher <olha.tryhub.work@gmail.com>
2025-04-11 18:52:40 +0300

    Update code.py

--- code.py
+++ code.py
@@ -1,44 +1,9 @@
-import pygame
-import time
-from random import randint
+# клас для об'єктів-картинок⬅️
+class Picture(Area):
+    def __init__(self, filename, x=0, y=0, width=10, height=10):
+        super().__init__(x=x, y=y, width=width, height=height,
+                         color=None)
+        self.image = pygame.image.load(filename)
 
-pygame.init()
-
-'''створюємо вікно програми'''
-
-back = (200, 255, 255)  # колір фону (background)
-mw = pygame.display.set_mode((500, 500))  # Вікно програми (main window)
-mw.fill(back)
-clock = pygame.time.Clock()
-
-'''клас прямокутник'''
-
-
-class Area():
-    def __init__(self, x=0, y=0, width=10, height=10, color=None):
-        self.rect = pygame.Rect(x, y, width, height)  # прямокутник
-        self.fill_color = color
-
-    def color(self, new_color):
-        self.fill_color = new_color
-
-    def fill(self):
-        pygame.draw.rect(mw, self.fill_color, self.rect)
-
-    def outline(self, frame_color, thickness):  # обведення існуючого прямокутника
-        pygame.draw.rect(mw, frame_color, self.rect, thickness)
-
-    def collidepoint(self, x, y):
-        return self.rect.collidepoint(x, y)
-
-
-'''клас напис'''
-
-
-class Label(Area):
-    def set_text(self, text, fsize=12, text_color=(0, 0, 0)):
-        self.image = pygame.font.SysFont('verdana', fsize).render(text, True, text_color)
-
-    def draw(self, shift_x=0, shift_y=0):
-        self.fill()
-        mw.blit(self.image, (self.rect.x + shift_x, self.rect.y + shift_y))
+    def draw(self):
+        mw.blit(self.image, (self.rect.x, self.rect.y))
