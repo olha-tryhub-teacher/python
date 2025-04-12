ffd53a31afbfe092b39b4e6de04ea3c3676c00e2
olha-tryhub-teacher <olha.tryhub.work@gmail.com>
2025-04-12 19:24:23 +0300

    Update code.py

--- code.py
+++ code.py
@@ -1,69 +1,16 @@
-from customtkinter import *
+def sum():
+    a = int(input("Введіть перше число: "))
+    b = int(input("Введіть введіть останнє число: "))
+
+    if a > b:
+        a, b = b, a
+
+    s = 0
+    for i in range(a, b + 1):
+        s += i
+
+    return s
 
 
-class MainWindow(CTk):
-    def __init__(self):
-        super().__init__()
-        self.geometry('400x300')
-        self.label = None
-        # menu frame
-        self.menu_frame = CTkFrame(self, width=30, height=300)
-        self.menu_frame.pack_propagate(False)
-        self.menu_frame.place(x=0, y=0)
-        self.is_show_menu = False
-        self.speed_animate_menu = -5
-        self.btn = CTkButton(self, text='▶️', command=self.toggle_show_menu, width=30)
-        self.btn.place(x=0, y=0)
-        # main
-        self.chat_field = CTkScrollableFrame(self)
-        self.chat_field.place(x=0, y=0)
-        self.message_entry = CTkEntry(self, placeholder_text='Введіть повідомлення:',
-                                      height=40)
-        self.message_entry.place(x=0, y=0)
-        self.send_button = CTkButton(self, text='>', width=50, height=40)
-        self.send_button.place(x=0, y=0)
-
-        self.adaptive_ui()
-
-    def toggle_show_menu(self):
-        if self.is_show_menu:
-            self.is_show_menu = False
-            self.speed_animate_menu *= -1
-            self.btn.configure(text='▶️')
-            self.show_menu()
-        else:
-            self.is_show_menu = True
-            self.speed_animate_menu *= -1
-            self.btn.configure(text='◀️')
-            self.show_menu()
-            # setting menu widgets
-            self.label = CTkLabel(self.menu_frame, text='Імʼя')
-            self.label.pack(pady=30)
-            self.entry = CTkEntry(self.menu_frame)
-            self.entry.pack()
-
-    def show_menu(self):
-        self.menu_frame.configure(width=self.menu_frame.winfo_width() + self.speed_animate_menu)
-        if not self.menu_frame.winfo_width() >= 200 and self.is_show_menu:
-            self.after(10, self.show_menu)
-        elif self.menu_frame.winfo_width() >= 40 and not self.is_show_menu:
-            self.after(10, self.show_menu)
-            if self.label and self.entry:
-                self.label.destroy()
-                self.entry.destroy()
-
-    def adaptive_ui(self):
-        self.menu_frame.configure(height=self.winfo_height())
-        self.chat_field.place(x=self.menu_frame.winfo_width())
-        self.chat_field.configure(width=self.winfo_width() - self.menu_frame.winfo_width() - 20,
-                                  height=self.winfo_height() - 40)
-        self.send_button.place(x=self.winfo_width() - 50, y=self.winfo_height() - 40)
-        self.message_entry.place(x=self.menu_frame.winfo_width(), y=self.send_button.winfo_y())
-        self.message_entry.configure(
-            width=self.winfo_width() - self.menu_frame.winfo_width() - self.send_button.winfo_width())
-
-        self.after(50, self.adaptive_ui)
-
-
-win = MainWindow()
-win.mainloop()
+r = sum()
+print(f"Сума діапазону дорівнює {r}")
