013c9d20d0f617fd5e84cfa81f2a39c65a14a7c4
olha-tryhub-teacher <olha.tryhub.work@gmail.com>
2025-04-19 19:36:56 +0300

    Update code.py

--- code.py
+++ code.py
@@ -1,4 +1,22 @@
-    def add_message(self, text):
-        label = CTkLabel(self.chat_field, text=text, anchor="w", justify="left", wraplength=300)
-        label.pack(fill="x", anchor="w", pady=2, padx=5)
-        self.chat_field._parent_canvas.yview_moveto(1.0)  # прокрутка вниз
+heating_on = False
+
+def reset():
+    global heating_on
+    heating_on = False
+
+def control():
+    global heating_on
+    t = float(input("Введіть вашу температуру: "))
+
+    if t < 18:
+        heating_on = True
+    elif t > 20:
+        heating_on = False
+
+control()
+print(heating_on)
+control()
+print(heating_on)
+
+reset()
+print(heating_on)
