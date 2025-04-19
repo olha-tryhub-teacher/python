dbbccd554c7c401f6afa5c6bdd2ee49abe1dc685
olha-tryhub-teacher <olha.tryhub.work@gmail.com>
2025-04-19 16:38:15 +0300

    Update code.py

--- code.py
+++ code.py
@@ -1,45 +1,69 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Olha Tryhub CV</title>
-</head>
-<body>
-<!--<img src="{{ url_for('static', filename='photo.jpg') }}">-->
-<h1>{{name}}</h1>
-<h3>{{position}}</h3>
-<h2>Contacts</h2>
-<ul>
-    {% for contact, link in contacts %}
-        {% if link %}
-            <li><a href={{link}}>{{contact}}</a></li>
-        {% else %}
-            <li>{{contact}}</li>
-        {% endif %}
-    {% endfor %}
-</ul>
+from customtkinter import *
 
-<h2>Summary</h2>
-<p>{{summary}}</p>
 
-<h2>Skills</h2>
-<table border="1">
-    {% for i in range(0, skills|length, 3) %}
-        <tr>
-            {% for j in range(3) %}
-                {% if i + j < skills|length %}
-                    <td>{{ skills[i + j] }}</td>
-                {% else %}
-                    <td></td>
-                {% endif %}
-            {% endfor %}
-        </tr>
-    {% endfor %}
-</table>
+class MainWindow(CTk):
+    def __init__(self):
+        super().__init__()
+        self.geometry('400x300')
+        self.label = None
+        # menu frame
+        self.menu_frame = CTkFrame(self, width=30, height=300)
+        self.menu_frame.pack_propagate(False)
+        self.menu_frame.place(x=0, y=0)
+        self.is_show_menu = False
+        self.speed_animate_menu = -5
+        self.btn = CTkButton(self, text='▶️', command=self.toggle_show_menu, width=30)
+        self.btn.place(x=0, y=0)
+        # main
+        self.chat_field = CTkScrollableFrame(self)
+        self.chat_field.place(x=0, y=0)
+        self.message_entry = CTkEntry(self, placeholder_text='Введіть повідомлення:',
+                                      height=40)
+        self.message_entry.place(x=0, y=0)
+        self.send_button = CTkButton(self, text='>', width=50, height=40)
+        self.send_button.place(x=0, y=0)
 
-<h2>Projects</h2>
-{% for project, link in projects %}
-    <a href={{link}}>{{project}}</a>
-{%endfor%}
-</body>
-</html>
+        self.adaptive_ui()
+
+    def toggle_show_menu(self):
+        if self.is_show_menu:
+            self.is_show_menu = False
+            self.speed_animate_menu *= -1
+            self.btn.configure(text='▶️')
+            self.show_menu()
+        else:
+            self.is_show_menu = True
+            self.speed_animate_menu *= -1
+            self.btn.configure(text='◀️')
+            self.show_menu()
+            # setting menu widgets
+            self.label = CTkLabel(self.menu_frame, text='Імʼя')
+            self.label.pack(pady=30)
+            self.entry = CTkEntry(self.menu_frame)
+            self.entry.pack()
+
+    def show_menu(self):
+        self.menu_frame.configure(width=self.menu_frame.winfo_width() + self.speed_animate_menu)
+        if not self.menu_frame.winfo_width() >= 200 and self.is_show_menu:
+            self.after(10, self.show_menu)
+        elif self.menu_frame.winfo_width() >= 40 and not self.is_show_menu:
+            self.after(10, self.show_menu)
+            if self.label and self.entry:
+                self.label.destroy()
+                self.entry.destroy()
+
+    def adaptive_ui(self):
+        self.menu_frame.configure(height=self.winfo_height())
+        self.chat_field.place(x=self.menu_frame.winfo_width())
+        self.chat_field.configure(width=self.winfo_width() - self.menu_frame.winfo_width() - 20,
+                                  height=self.winfo_height() - 40)
+        self.send_button.place(x=self.winfo_width() - 50, y=self.winfo_height() - 40)
+        self.message_entry.place(x=self.menu_frame.winfo_width(), y=self.send_button.winfo_y())
+        self.message_entry.configure(
+            width=self.winfo_width() - self.menu_frame.winfo_width() - self.send_button.winfo_width())
+
+        self.after(50, self.adaptive_ui)
+
+
+win = MainWindow()
+win.mainloop()
