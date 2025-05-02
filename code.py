290030517bb98b478da9ecaeaf07217ce25614fc
olha-tryhub-teacher <olha.tryhub.work@gmail.com>
2025-05-02 17:59:19 +0300

    Update code.py

--- code.py
+++ code.py
@@ -1,128 +1,156 @@
-from customtkinter import *
-import threading #⬅️
-from socket import * #⬅️
+# копіюємо із фаст клікеру створення вікна та класи Area, Label
+# прибрати зайві імпорти
+import pygame
+
+pygame.init()
+
+'''створюємо вікно програми'''
+
+back = (200, 255, 255)  # колір фону (background)
+mw = pygame.display.set_mode((500, 500))  # Вікно програми (main window)
+mw.fill(back)
+clock = pygame.time.Clock()
+
+# змінні, що відповідають за координати платформи
+racket_x = 200
+racket_y = 330
+# прапор закінчення гри
+game_over = False
+
+# змінні, які відповідають за напрями переміщення м'яча
+dx = 3
+dy = 3
+# фраги, які відповідають за рух платформи вправо/ліворуч
+move_right = False
+move_left = False
+
+'''клас прямокутник'''
 
 
-class MainWindow(CTk):
-    def __init__(self):
-        super().__init__()
-        self.geometry("400x300")
-        self.label = None
-        # menu frame
-        self.menu_frame = CTkFrame(self, width=30, height=300)
-        self.menu_frame.pack_propagate(False)
-        self.menu_frame.place(x=0, y=0)
-        self.is_show_menu = False
-        self.speed_animate_menu = -5
-        self.btn = CTkButton(self, text="▶️", command=self.toggle_show_menu, width=30)
-        self.btn.place(x=0, y=0)
-        # main
-        self.chat_field = CTkScrollableFrame(self)
-        self.chat_field.place(x=0, y=0)
-        self.message_entry = CTkEntry(self, placeholder_text="Введіть повідомлення:",
-                                      height=40)
-        self.message_entry.place(x=0, y=0)
-        self.send_button = CTkButton(self, text=">", width=50, height=40, command=self.send_message)  # add send comm⬅️
-        self.send_button.place(x=0, y=0)
-
-        self.username = "Владос"#⬅️
-
-        try:#⬅️
-            self.sock = socket(AF_INET, SOCK_STREAM)
-            self.sock.connect(("localhost", 8080))
-            hello = f"TEXT@{self.username}@[SYSTEM] {self.username} приєднався(лась) до чату!\n"
-            self.sock.send(hello.encode("utf-8"))
-            threading.Thread(target=self.recv_message, daemon=True).start()
-        except Exception as e:#⬅️
-            self.add_message(f"Не вдалося підключитися до сервера: {e}")
-
-        self.adaptive_ui()
-
-    def toggle_show_menu(self):
-        if self.is_show_menu:
-            self.is_show_menu = False
-            self.speed_animate_menu *= -1
-            self.btn.configure(text="▶️")
-            self.show_menu()
+class Area():
+    def __init__(self, x=0, y=0, width=10, height=10, color=None):
+        self.rect = pygame.Rect(x, y, width, height)  # прямокутник
+        if color:
+            self.fill_color = color
         else:
-            self.is_show_menu = True
-            self.speed_animate_menu *= -1
-            self.btn.configure(text="◀️")
-            self.show_menu()
-            # setting menu widgets
-            self.label = CTkLabel(self.menu_frame, text="Імʼя")
-            self.label.pack(pady=30)
-            self.entry = CTkEntry(self.menu_frame)
-            self.entry.pack()
+            self.fill_color = back
 
-    def show_menu(self):
-        self.menu_frame.configure(width=self.menu_frame.winfo_width() + self.speed_animate_menu)
-        if not self.menu_frame.winfo_width() >= 200 and self.is_show_menu:
-            self.after(10, self.show_menu)
-        elif self.menu_frame.winfo_width() >= 40 and not self.is_show_menu:
-            self.after(10, self.show_menu)
-            if self.label and self.entry:
-                self.label.destroy()
-                self.entry.destroy()
+    def color(self, new_color):
+        self.fill_color = new_color
 
-    def adaptive_ui(self):
-        self.menu_frame.configure(height=self.winfo_height())
-        self.chat_field.place(x=self.menu_frame.winfo_width())
-        self.chat_field.configure(width=self.winfo_width() - self.menu_frame.winfo_width() - 20,
-                                  height=self.winfo_height() - 40)
-        self.send_button.place(x=self.winfo_width() - 50, y=self.winfo_height() - 40)
-        self.message_entry.place(x=self.menu_frame.winfo_width(), y=self.send_button.winfo_y())
-        self.message_entry.configure(
-            width=self.winfo_width() - self.menu_frame.winfo_width() - self.send_button.winfo_width())
+    def fill(self):
+        pygame.draw.rect(mw, self.fill_color, self.rect)
 
-        self.after(50, self.adaptive_ui)
+    def outline(self, frame_color, thickness):  # обведення існуючого прямокутника
+        pygame.draw.rect(mw, frame_color, self.rect, thickness)
 
-    def add_message(self, text):
-        label = CTkLabel(self.chat_field, text=text, anchor="w", justify="left", wraplength=300)
-        label.pack(fill="x", anchor="w", pady=2, padx=5)
-        self.chat_field._parent_canvas.yview_moveto(1.0)  # прокрутка вниз
+    def collidepoint(self, x, y):
+        return self.rect.collidepoint(x, y)
 
-    def send_message(self):
-        message = self.message_entry.get()
-        if message:
-            self.add_message(f"{self.username}: {message}")
-            data = f"TEXT@{self.username}@{message}\n"
-            try:
-                self.sock.sendall(data.encode())
-            except:
-                pass
-        self.message_entry.delete(0, END)
-
-    def recv_message(self):
-        buffer = ""
-        while True:
-            try:
-                chunk = self.sock.recv(4096)
-                if not chunk:
-                    break
-                buffer += chunk.decode()
-
-                while "\n" in buffer:
-                    line, buffer = buffer.split("\n", 1)
-                    self.handle_line(line.strip())
-            except:
-                break
-        self.sock.close()
-
-    def handle_line(self, line):
-        if not line:
-            return
-        parts = line.split("@", 3)
-        msg_type = parts[0]
-
-        if msg_type == "TEXT":
-            if len(parts) >= 3:
-                author = parts[1]
-                message = parts[2]
-                self.add_message(f"{author}: {message}")
-        else:
-            self.add_message(line)
+    def colliderect(self, rect):  # новий метод для зіткнення
+        return self.rect.colliderect(rect)
 
 
-win = MainWindow()
-win.mainloop()
+'''клас напис'''
+
+
+class Label(Area):
+    def set_text(self, text, fsize=12, text_color=(0, 0, 0)):
+        self.image = pygame.font.SysFont('verdana', fsize).render(text, True, text_color)
+
+    def draw(self, shift_x=0, shift_y=0):
+        self.fill()
+        mw.blit(self.image, (self.rect.x + shift_x, self.rect.y + shift_y))
+
+
+# клас для об'єктів-картинок
+class Picture(Area):
+    def __init__(self, filename, x=0, y=0, width=10, height=10):
+        super().__init__(x=x, y=y, width=width, height=height,
+                         color=None)
+        self.image = pygame.image.load(filename)
+
+    def draw(self):
+        mw.blit(self.image, (self.rect.x, self.rect.y))
+
+
+# створення м'яча та платформи
+ball = Picture('ball.png', 160, 200, 50, 50)
+platform = Picture('platform.png', 200, 300, 100, 30)
+# Створення ворогів
+start_x = 5  # координати створення першого монстра
+start_y = 5
+count = 9  # кількість монстрів у верхньому ряду
+monsters = []  # список для зберігання об'єктів-монстрів
+for j in range(3):  # цикл по стовпцях
+    y = start_y + (55 * j)  # координата монстра у кожному слід. стовпці буде зміщена на 55 пікселів по y
+    x = start_x + (27.5 * j)  # і 27.5 по x
+    for i in range(count):  # цикл по рядах(рядків) створює в рядку кількість монстрів,що дорівнює count
+        d = Picture('enemy.png', x, y, 50, 50)  # створюємо монстра
+        monsters.append(d)  # додаємо до списку
+        x = x + 55  # збільшуємо координату наступного монстра
+    count = count - 1  # для наступного ряду зменшуємо кількість монстрів
+
+while not game_over:
+    ball.fill() # пишемо пізніше, як Оля скаже
+    platform.fill() # пишемо пізніше, як Оля скаже
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            game_over = True
+        # перевіряємо тип події і кнопку події
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_d:  # якщо натиснута клавіша
+                move_right = True  # піднімаємо прапор
+            if event.key == pygame.K_a:
+                move_left = True  # піднімаємо прапор
+        elif event.type == pygame.KEYUP:
+            if event.key == pygame.K_d:
+                move_right = False  # опускаємо прапор
+            if event.key == pygame.K_a:
+                move_left = False  # опускаємо прапор
+
+    if move_right:  # прапор руху вправо
+        platform.rect.x += 3
+    if move_left:  # прапор руху вліво
+        platform.rect.x -= 3
+
+    # додаємо постійне прискорення м'ячу по x і y
+    ball.rect.x += dx
+    ball.rect.y += dy
+    # якщо м'яч досягає меж екрана, міняємо напрямок його руху
+    if ball.rect.y < 0:
+        dy *= -1
+    if ball.rect.x > 450 or ball.rect.x < 0:
+        dx *= -1
+    # якщо м'яч торкнувся ракетки, міняємо напрямок руху
+    if ball.rect.colliderect(platform.rect):
+        dy *= -1
+
+    # перевірка поразки та перемоги⬅️⬅️⬅️
+    if ball.rect.y > 350:
+        time_text = Label(150, 150, 50, 50, back)
+        time_text.set_text('YOU LOSE', 60, (255, 0, 0))
+        time_text.draw(10, 10)
+        game_over = True
+
+    if len(monsters) == 0:
+        time_text = Label(150, 150, 50, 50, back)
+        time_text.set_text('YOU WIN', 60, (0, 200, 0))
+        time_text.draw(10, 10)
+        game_over = True
+
+    # малюємо всіх монстрів зі списку
+    for m in monsters:
+        m.draw()
+        # якщо монстра торкнувся м'яч, видаляємо монстра зі списку та міняємо напрямки руху м'яча⬅️⬅️⬅️
+        if m.rect.colliderect(ball.rect):
+            monsters.remove(m)
+            m.fill()
+            dy *= -1
+
+    platform.draw()
+    ball.draw()
+
+    pygame.display.update()
+    clock.tick(40)
