252d18e38b87896d9bac736b21221a2e3b06a87e
olha-tryhub-teacher <olha.tryhub.work@gmail.com>
2025-05-17 18:30:11 +0300

    Update code.py

--- code.py
+++ code.py
@@ -1,5 +1,42 @@
 from turtle import *
-from art import *
+
+def square(a, col):
+    color(col)
+    for i in range(4):
+        fd(a)
+        lt(90)
+
+
+def rectangle(a, b, col):
+    color(col)
+    for i in range(2):
+        fd(a)
+        lt(90)
+        fd(b)
+        lt(90)
+
+
+def triangle(a, col):
+    color(col)
+    for i in range(3):
+        fd(a)
+        lt(120)
+
+def parallelogram(a, b, col):
+    color(col)
+    for i in range(2):
+        fd(a)
+        lt(125)
+        fd(b)
+        lt(55)
+
+
+def polygon(a, n, col):
+    color(col)
+    angel = 360 / n
+    for i in range(n):
+        fd(a)
+        lt(angel)
 
 
 def square_fill(a, col):
