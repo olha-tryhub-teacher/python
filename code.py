d465e30c80c9f70a002190b71252adc78c64c6a2
olha-tryhub-teacher <olha.tryhub.work@gmail.com>
2025-05-17 19:26:11 +0300

    Update code.py

--- code.py
+++ code.py
@@ -1,1 +1,20 @@
-https://garticphone.com/uk/?c=03e12d75e3
+from turtle import *
+from art import *
+
+def cloud(x, y, a, col):
+  start(x, y)
+  w = a//6
+  start_line(w, col,col)
+  fd(a)
+  
+  start(x+a//5, y)
+  w = a//4
+  start_line(w, col,col)
+  fd(a - a//4)
+  
+  start(x+a//2, y)
+  w = a//3
+  start_line(w, col,col)
+  fd(a // 3)
+  
+cloud(0,0,100,"#0a279a")
