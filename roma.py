69a57609a77fe83a12f371248d929bb06ca225a5
olha-tryhub-teacher <olha.tryhub.work@gmail.com>
2025-05-21 18:51:37 +0300

    Update roma.py

--- roma.py
+++ roma.py
@@ -1,2 +1,4 @@
-bonus_ships = sprite.Group()
-bonus_spawn_time = timer()
+if timer() - bonus_spawn_time >= 10:
+    bonus = BonusShip(img_enemy2, 50, 50, randint(2, 4))  # використовується img_enemy2 або окрема картинка
+    bonus_ships.add(bonus)
+    bonus_spawn_time = timer()
