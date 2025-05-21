93d4329f843cbde110b3f8c53c51c88d036dcc40
olha-tryhub-teacher <olha.tryhub.work@gmail.com>
2025-05-21 16:33:19 +0300

    Update code.py

--- code.py
+++ code.py
@@ -1,20 +1,107 @@
-from turtle import *
-from art import *
+#підключи свій модуль art
+from turtle import*
 
-def cloud(x, y, a, col):
-  start(x, y)
-  w = a//6
-  start_line(w, col,col)
-  fd(a)
+def start(x, y):
+    penup()
+    goto(x, y)
+    pendown()
+
+
+def square(a, col):
+    color(col)
+    for i in range(4):
+        fd(a)
+        lt(90)
+
+
+def rectangle(a, b, col):
+    color(col)
+    for i in range(2):
+        fd(a)
+        lt(90)
+        fd(b)
+        lt(90)
+
+
+def triangle(a, col):
+    color(col)
+    for i in range(3):
+        fd(a)
+        lt(120)
+
+def parallelogram(a, b, col):
+    color(col)
+    for i in range(2):
+        fd(a)
+        lt(125)
+        fd(b)
+        lt(55)
+
+
+def polygon(a, n, col):
+    color(col)
+    angel = 360 / n
+    for i in range(n):
+        fd(a)
+        lt(angel)
+
+
+def circle(a, col):
+    color(col)
+    circle(a)
+
+
+def square_fill(a, col):
+  color(col)
+  begin_fill()
+  for i in range(4):
+    fd(a)
+    lt(90)
+  end_fill()
+    
+    
+def rectangle_fill(a,b, col):
+  color(col)
+  begin_fill()
+  for i in range(2):
+    fd(a)
+    lt(90)
+    fd(b)
+    lt(90)
+  end_fill()
   
-  start(x+a//5, y)
-  w = a//4
-  start_line(w, col,col)
-  fd(a - a//4)
   
-  start(x+a//2, y)
-  w = a//3
-  start_line(w, col,col)
-  fd(a // 3)
+def triangle_fill(a, col):
+  color(col)
+  begin_fill()
+  for i in range(3):
+    fd(a)
+    lt(120)
+  end_fill()
   
-cloud(0,0,100,"#0a279a")
+  
+def parallelogram_fill(a, b, col):
+  color(col)
+  begin_fill()
+  for i in range(2):
+    fd(a)
+    lt(125)
+    fd(b)
+    lt(55)
+  end_fill()
+  
+  
+def polygon_fill(a, n, col):
+  color(col)
+  angel = 360 / n
+  begin_fill()
+  for i in range(n):
+    fd(a)
+    lt(angel)
+  end_fill()
+  
+def circle_fill(a,col):
+  color(col)
+  begin_fill()
+  circle(a)
+  end_fill()
