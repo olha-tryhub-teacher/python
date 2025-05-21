09f6642125cc23882bf4a8f52fb5345445973dd5
olha-tryhub-teacher <olha.tryhub.work@gmail.com>
2025-05-21 18:53:02 +0300

    Update roma.py

--- roma.py
+++ roma.py
@@ -1,4 +1,5 @@
-if timer() - bonus_spawn_time >= 10:
-    bonus = BonusShip(img_enemy2, 50, 50, randint(2, 4))  # використовується img_enemy2 або окрема картинка
-    bonus_ships.add(bonus)
-    bonus_spawn_time = timer()
+if mode == "catch":
+    for bonus in bonus_ships:
+        if bonus.rect.collidepoint(x, y):
+            coins += 1
+            bonus.kill()
