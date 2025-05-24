bb7d08aec3c8ded42840c56ce8c0c900740b1a27
olha-tryhub-teacher <olha.tryhub.work@gmail.com>
2025-05-24 19:34:26 +0300

    Update code.py

--- code.py
+++ code.py
@@ -33,3 +33,25 @@ for line in pic_map:
         x+=pixel_size
     x = -100
     y -= pixel_size
+
+# 3.3
+
+pixel_sizes = 10
+coordinates = [-200,0,140,160,-20,50]
+pixel_size = 10
+
+def drawPix(x,y, pic_map, colors_map):
+    x_start = x
+    for line in pic_map:
+        for num in line:
+            start(x,y)
+            if num != "-":
+                squareFill(pixel_size,colors_map[int(num)])
+            x += pixel_size 
+        x = x_start
+        y -= pixel_size 
+start(-300,-300)
+squareFill(800,"#bbeef9")
+drawPix(coordinates[0],coordinates[1],pic_map1,colors_map1)
+drawPix(coordinates[2],coordinates[3],pic_map2,colors_map2)
+drawPix(coordinates[4],coordinates[5],pic_map3,colors_map3)
