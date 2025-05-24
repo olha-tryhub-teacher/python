5c3cfc72026d04f478c636be75bff94922312c02
olha-tryhub-teacher <olha.tryhub.work@gmail.com>
2025-05-24 19:09:36 +0300

    Update code.py

--- code.py
+++ code.py
@@ -1,107 +1,35 @@
-#підключи свій модуль art
-from turtle import*
+# юніт 2, завдання 3
 
-def start(x, y):
-    penup()
-    goto(x, y)
-    pendown()
-
-
-def square(a, col):
-    color(col)
-    for i in range(4):
-        fd(a)
-        lt(90)
+# Виводить список курсорів у стовпчик.
+def display_cursors(cursors):
+    for c in cursors:
+        print(c)
 
 
-def rectangle(a, b, col):
-    color(col)
-    for i in range(2):
-        fd(a)
-        lt(90)
-        fd(b)
-        lt(90)
+while True:
+    display_cursors(cursors)
+    ans = input("Який курсор видалити?")
+    if ans != "0":
+        if ans in cursors:
+            cursors.remove(ans)
+            print(f"Курсор '{ans}' видалено.")
+        else:
+            print("Такого курсора немає у списку. Спробуйте ще раз.")
+    else:
+        break
+print(cursors)
 
+# юніт 3
 
-def triangle(a, col):
-    color(col)
-    for i in range(3):
-        fd(a)
-        lt(120)
-
-def parallelogram(a, b, col):
-    color(col)
-    for i in range(2):
-        fd(a)
-        lt(125)
-        fd(b)
-        lt(55)
-
-
-def polygon(a, n, col):
-    color(col)
-    angel = 360 / n
-    for i in range(n):
-        fd(a)
-        lt(angel)
-
-
-def circle(a, col):
-    color(col)
-    circle(a)
-
-
-def square_fill(a, col):
-  color(col)
-  begin_fill()
-  for i in range(4):
-    fd(a)
-    lt(90)
-  end_fill()
-    
-    
-def rectangle_fill(a,b, col):
-  color(col)
-  begin_fill()
-  for i in range(2):
-    fd(a)
-    lt(90)
-    fd(b)
-    lt(90)
-  end_fill()
-  
-  
-def triangle_fill(a, col):
-  color(col)
-  begin_fill()
-  for i in range(3):
-    fd(a)
-    lt(120)
-  end_fill()
-  
-  
-def parallelogram_fill(a, b, col):
-  color(col)
-  begin_fill()
-  for i in range(2):
-    fd(a)
-    lt(125)
-    fd(b)
-    lt(55)
-  end_fill()
-  
-  
-def polygon_fill(a, n, col):
-  color(col)
-  angel = 360 / n
-  begin_fill()
-  for i in range(n):
-    fd(a)
-    lt(angel)
-  end_fill()
-  
-def circle_fill(a,col):
-  color(col)
-  begin_fill()
-  circle(a)
-  end_fill()
+pixel_size = 10
+x,y = -100,100
+for line in pic_map:
+    for num in line:
+        penup()
+        goto(x,y)
+        pendown()
+        c = colors_map[int(num)]
+        square_fill(pixel_size,c)
+        x+=pixel_size
+    x = -100
+    y -= pixel_size
