a7f9d93c23bf31cf9d7de27edef9a226d5e41b4b
olha-tryhub-teacher <olha.tryhub.work@gmail.com>
2025-05-31 19:17:16 +0300

    Update code.py

--- code.py
+++ code.py
@@ -1,15 +1,10 @@
-from art import *
-from turtle import *
-speed(100)
-pixel_size = 10
-x,y = -100,100
-for line in pic_map:
-    for num in line:
-        penup()
-        goto(x,y)
-        pendown()
-        c = colors_map[int(num)]
-        square_fill(pixel_size,c)
-        x+=pixel_size
-    x = -100
-    y -= pixel_size
+speed(0)
+hideturtle()
+
+
+def zero(x, y, size, col):
+    start(x + size // 2, y)
+    width(5)
+    setheading(0)
+    color(col)
+    circle(size // 2)
