1b592cf8cc80c026eac3f486f8ef478739c66dbf
olha-tryhub-teacher <olha.tryhub.work@gmail.com>
2025-06-07 19:16:16 +0300

    Update code.py

--- code.py
+++ code.py
@@ -1,14 +1,38 @@
-def writeWrong(letter):
-    color("black")
-    write(letter, font=("Arial", 28))
-    color("red")
-    width(2)
-    setheading(45)
-    fd(30)
-    setheading(180)
-    penup()
-    fd(20)
-    setheading(270 + 45)
-    pendown()
-    fd(30)
-    color("grey")
+from turtle import *
+from gallows import *
+from art import *
+from random import choice
+
+words = ["папуга", "школа", "карета", "відео", "намисто","шоколадка"]
+word = choice(words)
+countRight = 0
+countWrong = 0
+xWrong, yWrong = -170, 50
+
+# setup(1000, 800)
+
+writeAsk(word)
+
+while True:
+    letter = input("Введіть літеру:")
+    if letter in word:
+        c = writeRight(letter, word)
+        countRight += c
+    else:
+        penup()
+        goto(xWrong, yWrong)
+        pendown()
+        if xWrong < 50:
+            xWrong += 45
+        else:
+            xWrong, yWrong = -170, yWrong - 45
+        writeWrong(letter)
+        countWrong += 1
+        drawGalows(countWrong)
+
+    if countWrong == 12:
+        print("Lose")
+        break
+    if countRight == len(word):
+        print("Win")
+        break
