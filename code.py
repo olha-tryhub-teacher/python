1067d1aa72b9c43c733e3bf63fc4dfbed0475d0c
olha-tryhub-teacher <olha.tryhub.work@gmail.com>
2025-06-07 14:34:55 +0300

    Update code.py

--- code.py
+++ code.py
@@ -1,15 +1,39 @@
-v1 = Vector3D(1, 2, 3)
-v2 = Vector3D(4, 5, 6)
-
-v3 = v1 + v2
-print(f"Додавання векторів: {v3}")
-
-v4 = v1 - v2
-print(f"Віднімання векторів: {v4}")
-
-v5 = v1 * 2
-print(f"Множення вектора на скаляр: {v5}")
-
-print(f"Порівняння векторів v1 і v2: {v1 == v2}")  # Повинно вивести False
-
-print(f"{'-' * 35}\nВсього було створено {Vector3D.get_count()} векторів")
+class Vector3D:
+    _instances = 0
+    
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
+        Vector3D._instances += 1
+    
+    def __add__(self, other):
+        new_vector_x = self.x + other.x
+        new_vector_y = self.y + other.y
+        new_vector_z = self.z + other.z
+        return Vector3D(new_vector_x, new_vector_y, new_vector_z)
+    
+    def __sub__(self, other):
+        new_vector_x = self.x - other.x
+        new_vector_y = self.y - other.y
+        new_vector_z = self.z - other.z
+        return Vector3D(new_vector_x, new_vector_y, new_vector_z)
+        
+    def __mul__(self, n):
+        new_vector_x = self.x * n
+        new_vector_y = self.y * n
+        new_vector_z = self.z * n
+        return Vector3D(new_vector_x, new_vector_y, new_vector_z)
+        
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y and self.z == other.z
+    
+    def __str__(self):
+        return f"({self.x};{self.y};{self.z})"
+        
+    @staticmethod
+    def get_count():
+        return Vector3D._instances
+    
+    
+    
