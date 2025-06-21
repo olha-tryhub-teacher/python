507f935887ba5a0eb377b9962e7c975b00f789cb
olha-tryhub-teacher <olha.tryhub.work@gmail.com>
2025-06-21 12:39:39 +0300

    Update code.py

--- code.py
+++ code.py
@@ -1,8 +1,6 @@
-class Book():
-    def __init__(self, title, author, pages):
-        self.title = title
-        self.author = author
-        self.pages = pages
-
-    def __str__(self):
-        return f"{self.title} ({self.author}) - {self.pages} сторінок"
+books = [
+    Book("Колонія", "Макс Кідрук", 600),
+    Book("Ворошиловград", "Сергій Жадан", 200),
+    Book("Я бачу, вас цікавить пітьма", "Ілларіон Павлюк", 400),
+    Book("Я (Романтика)", "Микола Хвильовий", 200)
+]
