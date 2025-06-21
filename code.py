c569b185484dd322981c534d44fcab1a156301ae
olha-tryhub-teacher <olha.tryhub.work@gmail.com>
2025-06-21 12:53:42 +0300

    Update code.py

--- code.py
+++ code.py
@@ -1,7 +1,52 @@
-def generate_numbers(a, b):
-    for n in range(a, b + 1):
-        yield n + n - a + 1
+class Product:
+    def __init__(self, name, price, quantity):
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+
+    def __str__(self):
+        return f"{self.name}, Ціна: {self.price}, Кількість: {self.quantity}"
 
 
-for number in generate_numbers(3, 6):
-    print(number)
+class Store:
+    def __init__(self):
+        self.products = []
+
+    def add_product(self, product):
+        self.products.append(product)
+
+    def generate_product_list(self):
+        for product in self.products:
+            yield product
+
+
+def generate_product_page(product_generator, limit):
+    count = 0
+    for product in product_generator:
+        if count < limit:
+            print(product)
+            count += 1
+        if count == limit:
+            break
+
+
+# Створення магазину і додавання товарів
+store = Store()
+
+store.add_product(Product("Ліжко", 500, 5))
+store.add_product(Product("Стіл", 250, 3))
+store.add_product(Product("Стілець", 50, 10))
+store.add_product(Product("Шафа", 350, 4))
+store.add_product(Product("Диван", 800, 2))
+store.add_product(Product("Комод", 150, 6))
+store.add_product(Product("Столик", 100, 8))
+store.add_product(Product("Крісло", 75, 12))
+store.add_product(Product("Тумба", 55, 5))
+store.add_product(Product("Полиця", 200, 7))
+
+
+# Генератор для виведення товарів на сторінку сайту
+products_generated = store.generate_product_list()
+
+while input() == 'c':
+    generate_product_page(products_generated, limit=3)
