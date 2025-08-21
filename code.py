73fd6518c7818f3003ad1f8e3600f17eab42fc1f
olha-tryhub-teacher <olha.tryhub.work@gmail.com>
2025-08-21 16:41:09 +0300

    Update code.py

--- code.py
+++ code.py
@@ -1,100 +1,250 @@
-from fastapi import FastAPI, HTTPException
-from fastapi.responses import HTMLResponse
-from pydantic import BaseModel
-import json
-import os
+# модудь для гри, якмй будемо удосконалювати
 
-app = FastAPI()
+sdvig = 200
 
-DATA_FILE = "users.json"
+from turtle import *
 
-
-# ---------------------------------------------------
-# Допоміжні функції для роботи з файлом
-def load_users():
-    """Завантажуємо користувачів з JSON-файлу"""
-    if not os.path.exists(DATA_FILE):
-        return {}
-    with open(DATA_FILE, "r", encoding="utf-8") as f:
-        return json.load(f)
+coordinats = ["370;-100", "390;-100", "390;110", "340;110",
+              "300;110", "310;80", "305;20", "340;18",
+              "320;15", "330;15", "330;-41", "330;-41"]
 
 
-def save_users(users: dict):
-    """Зберігаємо користувачів у JSON-файл"""
-    with open(DATA_FILE, "w", encoding="utf-8") as f:
-        json.dump(users, f, ensure_ascii=False, indent=2)
-
-# ---------------------------------------------------
-# Модель користувача
-class User(BaseModel):
-    login: str
-    first_name: str
-    last_name: str
-    birth_year: int
+def start(x, y):
+    penup()
+    goto(x, y)
+    pendown()
 
 
-# ---------------------------------------------------
-# 1. Додавання нового користувача
+def square(a, col):
+    color(col)
+    for i in range(4):
+        fd(a)
+        lt(90)
 
 
-
-# 2. Редагування користувача
-@app.put("/user/edit/{login}")
-def edit_user(login: str, new_data: User):
-    users = load_users()
-    if login not in users:
-        raise HTTPException(status_code=404, detail="Користувача не знайдено")
-
-    users[login] = new_data.dict()
-    save_users(users)
-    return {"message": f"Користувача {login} оновлено", "data": new_data}
+def rectangle(a, b, col):
+    color(col)
+    for i in range(2):
+        fd(a)
+        lt(90)
+        fd(b)
+        lt(90)
 
 
-# ---------------------------------------------------
-# 3. Видалення користувача
-@app.delete("/user/delete/{login}")
-def delete_user(login: str):
-    users = load_users()
-    if login not in users:
-        raise HTTPException(status_code=404, detail="Користувача не знайдено")
-
-    del users[login]
-    save_users(users)
-    return {"message": f"Користувача {login} видалено"}
+def triangle(a, col):
+    color(col)
+    for i in range(3):
+        fd(a)
+        lt(120)
 
 
-# ---------------------------------------------------
-# 4. Отримання користувача за логіном
-@app.get("/user/{login}")
-def get_user(login: str):
-    users = load_users()
-    if login not in users:
-        raise HTTPException(status_code=404, detail="Користувача не знайдено")
-    return users[login]
+def drawGalows(count):
+    color("yellow")
+    width(10)
+    # 1
+    if count == 1:
+        start(100 + sdvig, 200)
+        goto(120 + sdvig, 150)
+        # 2
+    elif count == 2:
+        start(120 + sdvig, 150)
+        goto(120 + sdvig, 50)
+        # 3
+    elif count == 3:
+        start(120 + sdvig, 50)
+        goto(150 + sdvig, 0)
+        # 4
+    elif count == 4:
+        start(150 + sdvig, 0)
+        goto(100 + sdvig, -50)
+        # 5
+    elif count == 5:
+        start(100 + sdvig, -50)
+        goto(50 + sdvig, 0)
+        # 6
+    elif count == 6:
+        start(50 + sdvig, 0)
+        goto(80 + sdvig, 50)
+        # 7
+    elif count == 7:
+        start(80 + sdvig, 50)
+        goto(80 + sdvig, 150)
+
+        # 8
+    elif count == 8:
+        start(80 + sdvig, 150)
+        goto(100 + sdvig, 200)
+        # 9
+    elif count == 9:
+        start(100 + sdvig, -50)
+        goto(100 + sdvig, 0)
+        # 10
+    elif count == 10:
+        start(50 + sdvig, 0)
+        goto(100 + sdvig, 0)
+        # 11
+    elif count == 11:
+        start(100 + sdvig, 0)
+        goto(175 + sdvig, 50)
+
+        # 12
+    elif count == 12:
+        start(175 + sdvig, 50)
+        goto(150 + sdvig, 60)
+
+    elif count == 13:
+        start(150 + sdvig, 60)
+        goto(160 + sdvig, 150)
+
+    elif count == 14:
+        start(160 + sdvig, 150)
+        goto(200 + sdvig, 200)
+
+    elif count == 15:
+        start(200 + sdvig, 200)
+        goto(200 + sdvig, 0)
+
+    elif count == 16:
+        start(200 + sdvig, 0)
+        goto(0 + sdvig, 0)
+
+    elif count == 17:
+        start(0 + sdvig, 0)
+        goto(0 + sdvig, 200)
+
+    elif count == 18:
+        start(0 + sdvig, 200)
+        goto(40 + sdvig, 150)
+
+    elif count == 19:
+        start(40 + sdvig, 150)
+        goto(50 + sdvig, 60)
+
+    elif count == 20:
+        start(50 + sdvig, 60)
+        goto(25 + sdvig, 50)
+
+    elif count == 21:
+        start(25 + sdvig, 50)
+        goto(100 + sdvig, 0)
 
 
-# ---------------------------------------------------
-# 5. Отримання всіх користувачів (HTML)
-@app.get("/users")
-def get_all_users():
-    users = load_users()
-    if not users:
-        html = "<h3>Список користувачів порожній</h3>"
+def writeAsk(word):
+    start(-170, 100)
+    setheading(0)
+    width(4)
+    color("black")
+    for w in word:
+        fd(30)
+        penup()
+        fd(15)
+        pendown()
+
+
+def writeWrong(letter):
+    color("black")
+    write(letter, font=("Arial", 28))
+    color("red")
+    width(2)
+    setheading(45)
+    fd(30)
+    setheading(180)
+    penup()
+    fd(20)
+    setheading(270 + 45)
+    pendown()
+    fd(30)
+    color("grey")
+
+
+def writeRight(letter, word):
+    start(-170, 105)
+    penup()
+    color("black")
+    setheading(0)
+    count = 0
+    for w in word:
+        if w == letter:
+            pendown()
+            write(letter, font=("Arial", 32))
+            penup()
+            count += 1
+        fd(45)
+    return count
+
+
+def getCor(cor):
+    x, y = cor.split(";")  # "200;-100" -> ["200", "-100"]
+    x, y = int(x), int(y)
+    return x, y
+
+
+# сама гра, треба буде вставити у кружечок "Проєкт"
+# підключи молуль черепашки
+from turtle import *
+# підключи функцію фипадковго вибору з модуля випадковості
+from gerbenytsa import *
+# підключи свій модуль gowlls
+from random import choice
+
+# координати частин шибениці
+coordinats = ["370;-100", "390;-100", "390;110", "340;110",
+              "300;110", "310;80", "305;20", "340;18",
+              "320;15", "330;15", "330;-41", "330;-41"]
+
+# слова для гри, можна замінити на свої
+words = ["папуга", "школа", "карета", "відео", "намисто","шоколадка", "калина", "пшениця", "врожай", "тризуб", "карпати", "прапор", "жито", "соловей", "бандура", "барвінок"]
+
+
+# створи усі необхідні змінні
+def fon(col1, col2):
+    penup()
+    goto(-450, 200)
+    pendown()
+    width(450)
+    color(col1)
+    fd(900)
+
+    penup()
+    goto(-450, -200)
+    pendown()
+    width(400)
+    color(col2)
+    fd(900)
+
+
+# викличу функцію малювання завдання
+
+# ігровий цикл
+word = choice(words)
+countRight = 0
+countWrong = 0
+xWrong, yWrong = -170, 50
+speed(0)
+setup(1000, 700)
+fon("#005eb8", "#ffd101")
+writeAsk(word)
+
+while True:
+    letter = input("Введіть літеру:")
+    if letter in word:
+        c = writeRight(letter, word)
+        countRight += c
     else:
-        rows = ""
-        for login, data in users.items():
-            rows += f"<tr><td>{login}</td><td>{data['first_name']}</td><td>{data['last_name']}</td><td>{data['birth_year']}</td></tr>"
+        penup()
+        goto(xWrong, yWrong)
+        pendown()
+        if xWrong < 50:
+            xWrong += 45
+        else:
+            xWrong, yWrong = -170, yWrong - 45
+        writeWrong(letter)
+        countWrong += 1
+        drawGalows(countWrong)
 
-        html = f"""
-        <html>
-            <head><title>Список користувачів</title></head>
-            <body>
-                <h2>Усі користувачі</h2>
-                <table border="1">
-                    <tr><th>Логін</th><th>Ім'я</th><th>Прізвище</th><th>Рік народження</th></tr>
-                    {rows}
-                </table>
-            </body>
-        </html>
-        """
-    return HTMLResponse(content=html)
+    if countWrong == 21:
+        print("ne potughnyu progrash!")
+        break
+    if countRight == len(word):
+        print("potughna peremoga!")
+        break
