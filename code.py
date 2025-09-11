d185b74d3516b967af8df5f662e7097bb02c7416
olha-tryhub-teacher <olha.tryhub.work@gmail.com>
2025-09-11 17:16:39 +0300

    Update code.py

--- code.py
+++ code.py
@@ -1,12 +1,19 @@
-hideturtle()
+# ваш код
+# 1. Ігрове поле
+screen.bgcolor("black")
+pole = create_t(-150, 150, "turtle", "white")
+pole.ht()
+pole.begin_fill()
+for _ in range(4):
+    pole.fd(300)
+    pole.lt(90)
+pole.end_fill()
 
-
-def create_t(x, y, sh, col):
-    t = Turtle()
-    t.speed(0)
-    t.penup()
-    t.color(col)
-    t.shape(sh)
-    t.setheading(270)
-    t.goto(x, y)
-    return t
+# 2. Лейбли - лічильники
+# 2.1 Створити лейбли
+miss = create_t(-150, 160, "turtle", "red")
+miss.count = -1
+miss.ht()
+catch = create_t(70, 160, "turtle", "green")
+catch.count = -1
+catch.ht()
