8595fb4b031c353c2820f6f6040d1c8b7d95a71a
olha-tryhub-teacher <olha.tryhub.work@gmail.com>
2025-10-18 17:15:40 +0300

    Update code.py

--- code.py
+++ code.py
@@ -1,17 +1,55 @@
-from pygame import Rect
-from effects import draw_key_effect
+from pygame import *
+from settings import *
+from sounds import load_sounds
+from keys import draw_keys, create_key_rects
 
+init()
+screen = display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+display.set_caption("Piano Game")
 
-def draw_keys(screen, key_rects, pressed_keys):
-    """Малює клавіші, враховуючи які зараз натиснуті"""
-    for i, rect in enumerate(key_rects):
-        is_pressed = i in pressed_keys
-        draw_key_effect(screen, rect, is_pressed)
+sounds = load_sounds(KEYS)
+key_rects = create_key_rects(len(KEYS))
+keys_list = list(KEYS.keys())
+pressed_keys = set()
 
+running = True
+while running:
+    screen.fill(WHITE)
 
-def create_key_rects(num_keys, start_x=50, start_y=100, key_width=100, key_height=250):
-    rects = []
-    for i in range(num_keys):
-        x = start_x + i * key_width
-        rects.append(Rect(x, start_y, key_width, key_height))
-    return rects
+    # клавіші
+    draw_keys(screen, key_rects, pressed_keys)
+
+    display.flip()
+
+    for e in event.get():
+        if e.type == QUIT:
+            running = False
+
+        # клавіатура
+        if e.type == KEYDOWN:
+            k = key.name(e.key)
+            if k in sounds:
+                sounds[k].play()
+                idx = keys_list.index(k)
+                pressed_keys.add(idx)
+
+        if e.type == KEYUP:
+            k = key.name(e.key)
+            if k in sounds:
+                idx = keys_list.index(k)
+                if idx in pressed_keys:
+                    pressed_keys.remove(idx)
+
+        # миша по клавішах
+        if e.type == MOUSEBUTTONDOWN:
+            pos = e.pos
+            for i, rect in enumerate(key_rects):
+                if rect.collidepoint(pos):
+                    sounds[keys_list[i]].play()
+                    pressed_keys.add(i)
+
+        if e.type == MOUSEBUTTONUP:
+            pos = e.pos
+            for i, rect in enumerate(key_rects):
+                if i in pressed_keys and rect.collidepoint(pos):
+                    pressed_keys.remove(i)
