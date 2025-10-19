c051a6e0c3800044eb00774ef5b72b3e84bfc836
olha-tryhub-teacher <olha.tryhub.work@gmail.com>
2025-10-19 17:08:35 +0300

    Update code.py

--- code.py
+++ code.py
@@ -1,55 +1,24 @@
-from pygame import *
-from settings import *
-from sounds import load_sounds
-from keys import draw_keys, create_key_rects
 
-init()
-screen = display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
-display.set_caption("Piano Game")
+class Book():
+    def __init__(self, name, pages):
+        self.name = name
+        self.pages = pages
+        self.current_page = 1
 
-sounds = load_sounds(KEYS)
-key_rects = create_key_rects(len(KEYS))
-keys_list = list(KEYS.keys())
-pressed_keys = set()
+    def set_current_page(self, new_current_page):
+        if new_current_page <= self.pages:
+            self.current_page = new_current_page
+        else:
+            print(f"Номер сторінки занадто великий! Всього у книжці {self.pages} сторінок!")
 
-running = True
-while running:
-    screen.fill(WHITE)
+    def get_current_page(self):
+        print(f"Зараз ми на {self.current_page} сторінці")
 
-    # клавіші
-    draw_keys(screen, key_rects, pressed_keys)
 
-    display.flip()
+b1 = Book("title", 555)
 
-    for e in event.get():
-        if e.type == QUIT:
-            running = False
 
-        # клавіатура
-        if e.type == KEYDOWN:
-            k = key.name(e.key)
-            if k in sounds:
-                sounds[k].play()
-                idx = keys_list.index(k)
-                pressed_keys.add(idx)
-
-        if e.type == KEYUP:
-            k = key.name(e.key)
-            if k in sounds:
-                idx = keys_list.index(k)
-                if idx in pressed_keys:
-                    pressed_keys.remove(idx)
-
-        # миша по клавішах
-        if e.type == MOUSEBUTTONDOWN:
-            pos = e.pos
-            for i, rect in enumerate(key_rects):
-                if rect.collidepoint(pos):
-                    sounds[keys_list[i]].play()
-                    pressed_keys.add(i)
-
-        if e.type == MOUSEBUTTONUP:
-            pos = e.pos
-            for i, rect in enumerate(key_rects):
-                if i in pressed_keys and rect.collidepoint(pos):
-                    pressed_keys.remove(i)
+b1.get_current_page()
+b1.set_current_page(666)
+b1.set_current_page(23)
+b1.get_current_page()
