0bd00be9741ca5c444f1c1e0dc7448db19766f9c
olha-tryhub-teacher <olha.tryhub.work@gmail.com>
2025-11-06 17:23:23 +0200

    Update code.py

--- code.py
+++ code.py
@@ -1,19 +1,5 @@
-# клас для об'єктів-спрайтів
-class Sprite:
-    def __init__(self, coord, color):
-        self.image = pygame.Surface((100, 100))
-        self.image.fill(color)
-        self.rect = pygame.Rect(coord, (100, 100))
-        self.dx = 5
-
-    def update(self):
-        self.rect.centerx += self.dx
-        if self.rect.right > 500:
-            self.dx = -5  # змінити напрямок руху вліво
-        elif self.rect.left < 0:
-            self.dx = 5   # змінити напрямок руху вправо
-
-
-    def draw(self, surface):
-        # відображення зображення (image) на екрані
-        surface.blit(self.image, (self.rect.x, self.rect.y))
+sprites = [
+    Sprite((100, 100), RED),
+    Sprite((200, 150), GREEN),
+    Sprite((300, 250), BLUE)
+]
