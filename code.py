e3bd0e3730536ce5443f8dfb9018194f5c402dfb
olha-tryhub-teacher <olha.tryhub.work@gmail.com>
2025-11-08 19:01:28 +0200

    Update code.py

--- code.py
+++ code.py
@@ -1,19 +1,29 @@
-# реалізація переміщення та видалення об'єктів, що не взаємодіють з гравцем
-class Enviroment(Object):
-    def __init__(self, x, y):
-        super().__init__(x, y, img_ground)
-        self.cloud = Cloud()
-        self.cactus = Cactus()
+# ігрові об'єкти та ігровий цикл
+player = Player(100, 380)
+enviroment = Enviroment(0, 460)
 
-    def update(self):
-        self.rect.left -= SPEED
-        if self.rect.right <= 500:
-            self.rect.left = 0
+# створення годинника
+clock = pygame.time.Clock()
+running = True
 
-        self.cloud.update()
-        self.cactus.update()
+while running:
+    # обробка подій
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
 
-    def draw(self, screen):
-        screen.blit(self.img, (self.rect.left, self.rect.top))
-        self.cloud.draw(screen)
-        self.cactus.draw(screen)
+    # заливка екрана кольором, відображення прямокутників
+    screen.fill(WHITE)
+    player.update()
+
+    enviroment.update()
+    enviroment.draw(screen)
+
+    player.draw(screen)
+
+    # оновлення дисплея та обмеження частоти
+    pygame.display.flip()
+    clock.tick(FPS)
+
+pygame.quit()
