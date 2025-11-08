3410c39c6c2c577939e53ebb4457db98794f0241
olha-tryhub-teacher <olha.tryhub.work@gmail.com>
2025-11-08 19:07:36 +0200

    Update code.py

--- code.py
+++ code.py
@@ -1,3 +1,24 @@
+# реалізація переміщення та видалення об'єктів, що не взаємодіють з гравцем
+class Enviroment(Object):
+    def __init__(self, x, y):
+        super().__init__(x, y, img_ground)
+        self.cloud = Cloud()
+        self.cactus = Cactus()
+
+    def update(self):
+        self.rect.left -= SPEED
+        if self.rect.right <= 500:
+            self.rect.left = 0
+
+        self.cloud.update()
+        self.cactus.update()
+
+    def draw(self, screen):
+        screen.blit(self.img, (self.rect.left, self.rect.top))
+        self.cloud.draw(screen)
+        self.cactus.draw(screen)
+
+
 # ігрові об'єкти та ігровий цикл
 player = Player(100, 380)
 enviroment = Enviroment(0, 460)
