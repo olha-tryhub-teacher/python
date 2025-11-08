c06c542c0663589838939fd89fb4feeab9f5ee3c
olha-tryhub-teacher <olha.tryhub.work@gmail.com>
2025-11-08 19:21:52 +0200

    Update code.py

--- code.py
+++ code.py
@@ -1,3 +1,5 @@
-        self.imgs = [img_player, img_player2]
-        self.img_number = 0
-        self.anim_speed = 0.1
+    def animate(self):
+        self.img_number += self.anim_speed
+        if self.img_number >= len(self.imgs):
+            self.img_number = 0
+        self.img = self.imgs[int(self.img_number)]
