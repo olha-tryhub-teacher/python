ddf85b81606f21884e5935b2198190f98872ee3f
olha-tryhub-teacher <olha.tryhub.work@gmail.com>
2025-11-08 19:17:14 +0200

    Update code.py

--- code.py
+++ code.py
@@ -1,2 +1,3 @@
-img_player2 = pygame.image.load("DinoRun2.png").convert_alpha()
-img_player_jump = pygame.image.load("DinoJump.png").convert_alpha()
+        self.imgs = [img_player, img_player2]
+        self.img_number = 0
+        self.anim_speed = 0.1
