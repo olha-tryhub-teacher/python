45a175a4bfdfb5b78a4e6e4ad5066f8f8d0a8bee
olha-tryhub-teacher <olha.tryhub.work@gmail.com>
2025-11-20 16:52:18 +0200

    Update code.py

--- code.py
+++ code.py
@@ -1,15 +1,22 @@
-class Object:
-    def __init__(self, x, y, img):
-        self.img = img
-        self.rect = pygame.Rect(  # один рядок
-            x, y, self.img.get_width(),  # один рядок
-            self.img.get_height())  # один рядок
+# клас гравця з керуванням
+class Player(Object):
+    def __init__(self, x, y):
+        super().__init__(x, y, img_player)
+        self.max_y = y
+        self.velocity = 0
+        self.GRAVITY = 0.7
+        self.in_air = False
 
-    def draw(self, screen):
-        screen.blit(self.img, (self.rect.left, self.rect.top))
-
-class MovingObject(Object):
+    # реалізація керування і гравітації
     def update(self):
-        self.rect.left -= SPEED
-        if self.rect.right < 0:
-            self.rect.x = randint(600, 1000)
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_SPACE] and not self.in_air:
+            self.velocity = 15
+            self.in_air = True
+
+        if self.in_air:
+            self.rect.top -= self.velocity
+            self.velocity -= self.GRAVITY
+            if self.rect.top >= self.max_y:
+                self.in_air = False
+                self.rect.top = self.max_y
