3db8e61932af84edc5916c60b3c0c9b1e64c3f0c
olha-tryhub-teacher <olha.tryhub.work@gmail.com>
2025-11-20 17:02:03 +0200

    Update code.py

--- code.py
+++ code.py
@@ -1,4 +1,18 @@
+class Enviroment(Object):
+    def __init__(self, x, y):
+        super().__init__(x, y, img_ground)
+        self.cloud = Cloud()
+        self.cactus = Cactus()
+
     def update(self):
-        if self.rect.right <= 0:
-            self.rect.y = randint(50, 300)
-        super().update()
+        self.rect.left -= SPEED
+        if self.rect.right <= 500:
+            self.rect.left = 0
+
+        self.cloud.update()
+        self.cactus.update()
+
+    def draw(self, screen):
+        screen.blit(self.img, (self.rect.left, self.rect.top))
+        self.cloud.draw(screen)
+        self.cactus.draw(screen)
