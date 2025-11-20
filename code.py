5642d54423f8e40a234828a2be12c328163249da
olha-tryhub-teacher <olha.tryhub.work@gmail.com>
2025-11-20 16:57:56 +0200

    Update code.py

--- code.py
+++ code.py
@@ -1,22 +1,13 @@
-# клас гравця з керуванням
-class Player(Object):
-    def __init__(self, x, y):
-        super().__init__(x, y, img_player)
-        self.max_y = y
-        self.velocity = 0
-        self.GRAVITY = 0.7
-        self.in_air = False
+# кактус, що рухаються ліворуч
+class Cactus(MovingObject):
+    def __init__(self):
+        x = randint(600, 1000)
+        super().__init__(x, 400, img_cactus)
 
-    # реалізація керування і гравітації
-    def update(self):
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_SPACE] and not self.in_air:
-            self.velocity = 15
-            self.in_air = True
 
-        if self.in_air:
-            self.rect.top -= self.velocity
-            self.velocity -= self.GRAVITY
-            if self.rect.top >= self.max_y:
-                self.in_air = False
-                self.rect.top = self.max_y
+# хмара
+class Cloud(MovingObject):
+    def __init__(self):
+        x = randint(500, 1000)
+        y = randint(80, 200)
+        super().__init__(x, y, img_cloud)
