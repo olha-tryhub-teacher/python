300505baa8b08b0fbdd566b73b49f4ec65dbde45
olha-tryhub-teacher <olha.tryhub.work@gmail.com>
2025-11-20 16:59:29 +0200

    Update code.py

--- code.py
+++ code.py
@@ -1,13 +1,4 @@
-# кактус, що рухаються ліворуч
-class Cactus(MovingObject):
-    def __init__(self):
-        x = randint(600, 1000)
-        super().__init__(x, 400, img_cactus)
-
-
-# хмара
-class Cloud(MovingObject):
-    def __init__(self):
-        x = randint(500, 1000)
-        y = randint(80, 200)
-        super().__init__(x, y, img_cloud)
+    def update(self):
+        if self.rect.right <= 0:
+            self.rect.y = randint(50, 300)
+        super().update()
