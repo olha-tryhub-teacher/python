727bd6c7f8e5d21acb8ce0d9345f7417adb373dd
olha-tryhub-teacher <olha.tryhub.work@gmail.com>
2025-11-22 18:47:55 +0200

    Update code.py

--- code.py
+++ code.py
@@ -1,155 +1,130 @@
-# підключення pygame
-import pygame
-from random import randint
+import pygame as pg
 
-# кольори
-YELLOW = (200, 200, 0)
 BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
-BLUE = (0, 0, 255)
-GREEN = (0, 255, 0)
-RED = (255, 0, 0)
+WIDTH = 500
+HEIGHT = 500
 
-FPS = 50
-SPEED = 5
-# налаштування Pygame
-pygame.init()
-screen = pygame.display.set_mode((500, 500))
+pg.init()
+screen = pg.display.set_mode((WIDTH, HEIGHT))
 
-img_player = pygame.image.load("DinoRun1.png")
-img_player2 = pygame.image.load("DinoRun2.png")
-img_player_jump = pygame.image.load("DinoJump.png")
-
-img_cactus = pygame.image.load("SmallCactus1.png")
-img_cloud = pygame.image.load("Cloud.png")
-img_ground = pygame.image.load("Track.png")
+# завантаження картинок
+image_ball = pg.image.load("Ball.png").convert_alpha()
+image_block1 = pg.image.load("Block1.png").convert_alpha()
+image_block2 = pg.image.load("Block2.png").convert_alpha()
+image_block3 = pg.image.load("Block3.png").convert_alpha()
+image_platform1 = pg.image.load("Platform1.png").convert_alpha()
+image_bg = pg.image.load("background.png").convert()
 
 
-class Object:
-    def __init__(self, x, y, img):
-        self.img = img
-        self.rect = pygame.Rect(  # один рядок
-            x, y, self.img.get_width(),  # один рядок
-            self.img.get_height())  # один рядок
+class TextLabel:
+    def __init__(self, x, y, size=32, color=BLACK):
+        self.x = x
+        self.y = y
+        self.color = color
+        self.image = None
+        self.font = pg.font.Font(None, size)
 
-    def draw(self, screen):
-        screen.blit(self.img, (self.rect.left, self.rect.top))
-
-# клас гравця з керуванням
-class Player(Object):
-    def __init__(self, x, y):
-        super().__init__(x, y, img_player)
-        self.max_y = y
-        self.velocity = 0
-        self.GRAVITY = 0.7
-        self.in_air = False
-
-        self.imgs = [img_player, img_player2]
-        self.img_number = 0
-        self.anim_speed = 0.1
-
-    # реалізація керування і гравітації
-    def update(self):
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_SPACE] and not self.in_air:
-            self.velocity = 15
-            self.in_air = True
-            self.img = img_player_jump # ⬅️⬅️⬅️
-
-        if self.in_air:
-            self.rect.top -= self.velocity
-            self.velocity -= self.GRAVITY
-            if self.rect.top >= self.max_y:
-                self.in_air = False
-                self.rect.top = self.max_y
-        else:
-            self.animate()
-
-    def animate(self):
-        self.img_number += self.anim_speed
-        if self.img_number >= len(self.imgs):
-            self.img_number = 0
-        self.img = self.imgs[int(self.img_number)]
-
-
-class MovingObject(Object):
-    def update(self):
-        self.rect.left -= SPEED
-        if self.rect.right < 0:
-            self.rect.x = randint(600, 1000)
-
-
-# кактус, що рухаються ліворуч
-class Cactus(MovingObject):
-    def __init__(self):
-        x = randint(600, 1000)
-        super().__init__(x, 400, img_cactus)
-
-
-# хмара
-class Cloud(MovingObject):
-    def __init__(self):
-        x = randint(500, 1000)
-        y = randint(80, 200)
-        super().__init__(x, y, img_cloud)
-
-    def update(self):
-        if self.rect.right <= 0:
-            self.rect.y = randint(50, 300)
-        super().update()
-
-
-# реалізація переміщення та видалення об'єктів, що не взаємодіють з гравцем
-class Enviroment(Object):
-    def __init__(self, x, y):
-        super().__init__(x, y, img_ground)
-        self.cloud = Cloud()
-        self.cactus = Cactus()
-
-    def update(self):
-        self.rect.left -= SPEED
-        if self.rect.right <= 500:
-            self.rect.left = 0
-
-        self.cloud.update()
-        self.cactus.update()
+    def set_text(self, text):
+        self.image = self.font.render(text, True, self.color)
 
     def draw(self, screen):
-        screen.blit(self.img, (self.rect.left, self.rect.top))
-        self.cloud.draw(screen)
-        self.cactus.draw(screen)
+        screen.blit(self.image, (self.x, self.y))
 
 
-##################################
-# ігрові об'єкти та ігровий цикл
-player = Player(100, 380)
-enviroment = Enviroment(0, 460)
+class Sprite:
+    def __init__(self, x, y, image):
+        self.image = image
+        self.rect = pg.Rect(x, y, self.image.get_width(), self.image.get_height())
 
-# створення годинника
-clock = pygame.time.Clock()
+    def collide(self, sprite):
+        return self.rect.colliderect(sprite.rect)
+
+    def draw(self, screen):
+        screen.blit(self.image, (self.rect.left, self.rect.top))
+
+
+class Player(Sprite):
+    def __init__(self, x, y, image_base):
+        super().__init__(x, y, image_base)
+
+    def update(self):
+        keys = pg.key.get_pressed()
+        if keys[pg.K_LEFT]:
+            self.rect.left -= 8
+        if keys[pg.K_RIGHT]:
+            self.rect.left += 8
+
+
+class Ball(Sprite):
+    def __init__(self, x, y, image, velocity):
+        super().__init__(x, y, image)
+        self.SPEED = velocity
+        self.velocityx = velocity
+        self.velocityy = -velocity
+
+    def update(self):
+        self.rect.left += self.velocityx
+        self.rect.top += self.velocityy
+
+        if self.rect.left < 0 or self.rect.right > WIDTH:
+            self.velocityx = -self.velocityx
+        if self.rect.top < 0:
+            self.velocityy = self.SPEED
+
+    def change_velocity(self, player):
+        self.velocityy = -self.SPEED
+        # горизонтальна швидкість не змінюється
+
+
+# створення об'єктів гри
+ball = Ball(100, HEIGHT//2, image_ball, 4)
+player = Player(WIDTH//2 - 50, HEIGHT - 60, image_platform1)
+
+score = 0
+label = TextLabel(370, 470, 28)
+label.set_text(f"Score: {score}")
+
+blocks = []
+for i in range(15):
+    row = i // 5
+    col = i % 5
+    blocks.append(Sprite(50 + col*80, 20 + row*40,
+                  [image_block1, image_block2, image_block3][row]))
+
+clock = pg.time.Clock()
 running = True
 
 while running:
-    # обробка подій
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            exit()
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            running = False
 
-    if player.rect.colliderect(enviroment.cactus.rect):
-        running = False
-
-    # заливка екрана кольором, відображення прямокутників
-    screen.fill(WHITE)
+    ball.update()
     player.update()
 
-    enviroment.update()
-    enviroment.draw(screen)
+    # перевірка програшу
+    if ball.rect.top > HEIGHT:
+        print("Game Over!")
+        running = False
 
+    if player.collide(ball):
+        ball.change_velocity(player)
+
+    screen.blit(image_bg, (0, 0))
+
+    for b in blocks[:]:
+        if b.collide(ball):
+            blocks.remove(b)
+            ball.velocityy = ball.SPEED
+            score += 100
+            label.set_text(f"Score: {score}")
+        b.draw(screen)
+
+    ball.draw(screen)
+    label.draw(screen)
     player.draw(screen)
 
-    # оновлення дисплея та обмеження частоти
-    pygame.display.flip()
-    clock.tick(FPS)
+    pg.display.flip()
+    clock.tick(50)
 
-pygame.quit()
+pg.quit()
