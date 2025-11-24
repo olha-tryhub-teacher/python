b44394c576ac798bd296eab60ea4be41225d00b9
olha-tryhub-teacher <olha.tryhub.work@gmail.com>
2025-11-24 18:17:25 +0200

    Update code.py

--- code.py
+++ code.py
@@ -1,1 +1,24 @@
-<script src="{% static 'js/bootstrap.bundle.min.js' %}"></script>
+from django import forms
+from django.contrib.auth.models import User
+
+class RegisterForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput)
+    password2 = forms.CharField(label="Repeat password", widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ["username"]
+
+    def clean(self):
+        cleaned_data = super().clean()
+        p1 = cleaned_data.get("password")
+        p2 = cleaned_data.get("password2")
+
+        if p1 and p2 and p1 != p2:
+            raise forms.ValidationError("Passwords do not match!")
+
+        return cleaned_data
+
+class LoginForm(forms.Form):
+    username = forms.CharField(max_length=150)
+    password = forms.CharField(widget=forms.PasswordInput)
