79abf257adf50ed216c09d060be94a190fe016c8
olha-tryhub-teacher <olha.tryhub.work@gmail.com>
2025-12-08 19:22:46 +0200

    Update code.py

--- code.py
+++ code.py
@@ -1,51 +1,148 @@
-from django.db import models
+from django.shortcuts import render, get_object_or_404, redirect
+from django.urls import reverse_lazy
+from tasks import models
+from django.views.generic import ListView, DetailView, CreateView, View, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from tasks.forms import TaskForm, TaskFilterForm, CommentForm
+from django.http import HttpResponseRedirect
+from django.core.exceptions import PermissionDenied
+from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.auth.models import User
-from django.urls import reverse
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+
+from tasks.mixins import UserIsOwnerMixin
 
 
-class Task(models.Model):
+class TaskListView(ListView):
+    model = models.Task
+    context_object_name = "tasks"
+    template_name = "tasks/task_list.html"
 
-    STATUS_CHOICES = [
-        ("todo", "To Do"),
-        ("in_progress", "In Progress"),
-        ("done", "Done"),
-    ]
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        status = self.request.GET.get("status", "")
+        if status:
+            queryset = queryset.filter(status=status)
+        return queryset
 
-    PRIORITY_CHOICES = [
-        ("low", "Low"),
-        ("medium", "Medium"),
-        ("high", "High"),
-    ]
-
-    title = models.CharField(max_length=256)
-    description = models.TextField()
-    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default="todo")
-    priority = models.CharField(max_length=20, choices=PRIORITY_CHOICES, default="medium")
-    due_date = models.DateField(null=True, blank=True)
-    creator = models.ForeignKey(User, on_delete=models.CASCADE, related_name="tasks")
-
-    def __str__(self):
-        return self.title
-
-    def get_absolute_url(self):
-        return reverse('tasks:task-detail', kwargs={'pk': self.pk})
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["form"] = TaskFilterForm(self.request.GET)
+        return context
 
 
-class Comment(models.Model):
-    task = models.ForeignKey(Task, on_delete=models.CASCADE, related_name='comments')
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')
-    content = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    media = models.FileField(upload_to="comments_media/", blank=True, null=True)
+class TaskDetailView(LoginRequiredMixin, DetailView):
+    model = models.Task
+    context_object_name = "task"
+    template_name = 'tasks/task_detail.html'
 
-    def get_absolute_url(self):
-        return self.task.get_absolute_url()
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['comment_form'] = CommentForm()  # Додаємо порожню форму коментаря в контекст
+        return context
+
+    def post(self, request, *args, **kwargs):
+        comment_form = CommentForm(request.POST, request.FILES)
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.author = request.user
+            comment.task = self.get_object()
+            comment.save()
+            return redirect('tasks:task-detail', pk=comment.task.pk)
+        else:
+            # Тут можна обробити випадок з невалідною формою
+            pass
 
 
-class Like(models.Model):
-    comment = models.ForeignKey(Comment, on_delete=models.CASCADE, related_name='likes')
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='liked_comments')
-    created_at = models.DateTimeField(auto_now_add=True)
+class TaskCreateView(LoginRequiredMixin, CreateView):
+    model = models.Task
+    template_name = "tasks/task_form.html"
+    form_class = TaskForm
+    success_url = reverse_lazy("tasks:task-list")
 
-    class Meta:
-        unique_together = ('comment', 'user')  # Забезпечує унікальність лайків
+    def form_valid(self, form):
+        form.instance.creator = self.request.user
+        return super().form_valid(form)
+
+
+class TaskCompleteView(LoginRequiredMixin, UserIsOwnerMixin, View):
+    def post(self, request, *args, **kwargs):
+        task = self.get_object()
+        task.status = "done"
+        task.save()
+        return HttpResponseRedirect(reverse_lazy("tasks:task-list"))
+
+    def get_object(self):
+        task_id = self.kwargs.get("pk")
+        return get_object_or_404(models.Task, pk=task_id)
+
+
+class TaskUpdateView(LoginRequiredMixin, UserIsOwnerMixin, UpdateView):
+    model = models.Task
+    form_class = TaskForm
+    template_name = "tasks/task_update_form.html"
+    success_url = reverse_lazy("tasks:task-list")
+
+
+class TaskDeleteView(LoginRequiredMixin, UserIsOwnerMixin, DeleteView):
+    model = models.Task
+    success_url = reverse_lazy("tasks:task-list")
+    template_name = "tasks/task_delete_confirmation.html"
+
+
+class CommentUpdateView(LoginRequiredMixin, UpdateView):
+    model = models.Comment
+    fields = ['content']
+    template_name = 'tasks/edit_comment.html'
+
+    def form_valid(self, form):
+        comment = self.get_object()
+        if comment.author != self.request.user:
+            raise PermissionDenied("Ви не можете редагувати цей коментар.")
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse_lazy('tasks:task_detail', kwargs={'pk': self.object.task.pk})
+
+
+class CommentDeleteView(LoginRequiredMixin, DeleteView):
+    model = models.Comment
+    template_name = 'tasks/delete_comment.html'
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        return queryset.filter(author=self.request.user)
+
+    def get_success_url(self):
+        return reverse_lazy('tasks:task_detail', kwargs={'pk': self.object.task.pk})
+
+
+class CommentLikeToggle(LoginRequiredMixin, View):
+    def post(self, request, *args, **kwargs):
+        comment = get_object_or_404(models.Comment, pk=self.kwargs.get('pk'))
+        like_qs = models.Like.objects.filter(comment=comment, user=request.user)
+        if like_qs.exists():
+            like_qs.delete()
+        else:
+            models.Like.objects.create(comment=comment, user=request.user)
+        return HttpResponseRedirect(comment.get_absolute_url())
+
+
+class CustomLoginView(LoginView):
+    template_name = "tasks/login.html"
+    redirect_authenticated_user = True
+
+
+class CustomLogoutView(LogoutView):
+    next_page = "tasks:login"
+
+
+class RegisterView(CreateView):
+    template_name = "tasks/register.html"
+    form_class = UserCreationForm
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return redirect(reverse_lazy("tasks:login"))
