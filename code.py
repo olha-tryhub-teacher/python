d5ccfbf8af1953d3f7e4cce7ff50bae2e585e1c7
olha-tryhub-teacher <olha.tryhub.work@gmail.com>
2025-12-16 18:49:53 +0200

    Update code.py

--- code.py
+++ code.py
@@ -1,2 +1,5 @@
-GREEN = (0, 255, 0)  # ⬅️
-RED = (255, 0, 0)  # ⬅️
+label_win = TextLabel(100, 250, 50, GREEN)  # ⬅️
+label_win.set_text(f"WIN!!!")  # ⬅️
+
+label_lose = TextLabel(100, 250, 50, RED)  # ⬅️
+label_lose.set_text(f"LOSE!!!")  # ⬅️
