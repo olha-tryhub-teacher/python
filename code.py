23a4e24d444ddb463ae9a317b762ee0785322898
olha-tryhub-teacher <olha.tryhub.work@gmail.com>
2025-12-27 15:30:13 +0200

    Update code.py

--- code.py
+++ code.py
@@ -1,8 +1,11 @@
-    # ⬇️⬇️⬇️
-    def draw_center(self, scale):
-        draw.circle(
-            window,
-            self.color,
-            (WINDOW_SIZE[0] // 2, WINDOW_SIZE[1] // 2),
-            int(self.radius * scale)
-        )
+    # Кульки
+    to_remove = []
+    for ball in balls:
+        if player.collidecircle(ball):
+            to_remove.append(ball)
+            player.radius += int(ball.radius * 0.2)
+        else:
+            ball.reset(player.x, player.y, scale)
+
+    for ball in to_remove:
+        balls.remove(ball)
