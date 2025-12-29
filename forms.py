f5cd4bfcaf8a363891f8d64b19433fe34120b2d0
olha-tryhub-teacher <olha.tryhub.work@gmail.com>
2025-12-29 19:32:52 +0200

    Update forms.py

--- forms.py
+++ forms.py
@@ -1,19 +1,12 @@
-# Імпортуємо виняток PermissionDenied з Django,
-# який використовується для заборони доступу (HTTP 403)
-from django.core.exceptions import PermissionDenied
+from django.contrib import admin
+from django.urls import path, include
+from django.contrib.auth import views as auth_views
+from django.conf.urls.static import static
+from django.conf import settings
 
 
-# Оголошуємо міксін, який перевіряє, чи є користувач власником обʼєкта
-class UserIsOwnerMixin(object):
-    # Перевизначаємо метод dispatch,
-    # який викликається перед обробкою будь-якого HTTP-запиту (GET, POST тощо)
-    def dispatch(self, request, *args, **kwargs):
-        # Отримуємо обʼєкт, з яким працює view (наприклад, модель)
-        instance = self.get_object()
-        # Перевіряємо, чи поточний користувач є творцем (власником) обʼєкта
-        if instance.creator != self.request.user:
-            # Якщо ні — забороняємо доступ і повертаємо помилку 403
-            raise PermissionDenied
-        # Якщо перевірка пройшла успішно,
-        # передаємо керування стандартному dispatch методу
-        return super().dispatch(request, *args, **kwargs)
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path("", include("tasks.urls")),
+    path("accounts/login/", auth_views.LoginView.as_view(), name="login")
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
