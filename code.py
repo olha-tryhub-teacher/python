b43f7bb57890a354e0543819a2ac3f05c32d8a93
olha-tryhub-teacher <olha.tryhub.work@gmail.com>
2026-02-14 17:26:24 +0200

    Update code.py

--- code.py
+++ code.py
@@ -1,11 +1,12 @@
-    # Кульки
-    to_remove = []
-    for ball in balls:
-        if player.collidecircle(ball):
-            to_remove.append(ball)
-            player.radius += int(ball.radius * 0.2)
-        else:
-            ball.reset(player.x, player.y, scale)
+from socket import *
 
-    for ball in to_remove:
-        balls.remove(ball)
+client_socket = socket(AF_INET, SOCK_STREAM)
+client_socket.connect(('localhost', 12345))
+
+name = input("Для підключення на сервер введіть своє ім'я: ")
+client_socket.send(name.encode())
+
+print(client_socket.recv(1024).decode()) # отримали вітання
+
+client_socket.close()
+
