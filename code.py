194b73cb766f3ebfa62add4b5eef1e114d99b8b1
olha-tryhub-teacher <olha.tryhub.work@gmail.com>
2026-02-21 15:58:37 +0200

    Update code.py

--- code.py
+++ code.py
@@ -1,73 +1,12 @@
-# ваш код
-from turtle import *
-from random import randint
+from socket import *
 
-hideturtle()
+client_socket = socket(AF_INET, SOCK_STREAM)
+client_socket.connect(('localhost', 12345))
 
+name = input("Для підключення на сервер введіть своє ім'я: ")
+client_socket.send(name.encode())
 
-def create_t(x, y, h, col):
-    t = Turtle()
-    t.color(col)
-    t.shape("turtle")
-    t.setheading(h)
-    t.penup()
-    t.goto(x, y)
-    t.pendown()
-    return t
+print(client_socket.recv(1024).decode()) # отримали вітання
 
+client_socket.close()
 
-# 1.Ігрове поле
-screen = getscreen()
-screen.bgcolor("yellow")
-
-# 2.Лейбли-лічильник
-click = create_t(200, 100, 0, "blue")
-click.count = 0
-click.hideturtle()
-click.write(f"Click: {click.count}", font=("Arial", 24))
-
-# те саме тільки раннер
-runner = create_t(200, 150, 0, "red")
-runner.count = 0
-runner.hideturtle()
-runner.write(f"Runner: {runner.count}", font=("Arial", 24))
-
-# 3.Черепашки-втікачки
-# 3.1 створити черепашок
-t1 = create_t(0, 0, 0, "pink")
-
-
-# 3.2 функції-обробники кліку
-def click1(x, y):
-    t1.lt(randint(30, 200))
-    click.count += 1
-    click.clear()
-    click.write(f"Click: {click.count}", font=("Arial", 24))
-
-
-# 3.3 підписка на подію клік по черепашках
-t1.onclick(click1)
-
-# 4. Ігровий цикл
-while click.count < 20 and runner.count < 4:
-    if abs(t1.xcor()) < 200 and abs(t1.ycor()) < 200:
-        t1.forward(2)
-    else:
-        runner.count += 1
-        runner.clear()
-        runner.write(f"Runner: {runner.count}", font=("Arial", 24))
-
-
-# 5. Переврка виграшу/програшу
-if click.count >= 20:
-    click.penup()
-    click.goto(0, 0)
-    click.write("You winn", font=("Arial", 40))
-# те саме тільки раннер
-if runner.count >= 4:
-    runner.penup()
-    runner.goto(0, 0)
-    runner.write("You lose", font=("Arial", 40))
-
-
-done()
