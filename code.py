21a6307fd8756b43f6104055b6ed19b05a8b7942
olha-tryhub-teacher <olha.tryhub.work@gmail.com>
2026-03-21 19:26:38 +0200

    Update code.py

--- code.py
+++ code.py
@@ -1,23 +1,15 @@
-from customtkinter import *
-
-window = CTk()
-
-btn_color = "black"
-text_color = "white"
-
-for i in range(8):
-    for j in range(8):
-        btn = CTkButton(window, width=70, height=70)
-        btn.grid(column=i, row=j)
-
-        if (i + j) % 2 == 0:
-            btn_color = "black"
-            text_color = "white"
-        else:
-            btn_color = "white"
-            text_color = "black"
-
-        btn.configure(fg_color=btn_color, text_color=text_color)
-
-
-window.mainloop()
+direction = input("Куди рухається Pac-Man?").lower()
+move = int(input("Що поруч - введіть число"))
+if direction == "ліворуч" or direction == "праворуч" or direction == "вниз" or direction == "вгору":
+    if move == 0:
+        print(f"Pac-Man може рухатись {direction}.")
+    elif move == 1:
+        print(f"Pac-Man не може рухатись {direction}.")
+    elif move == 2:
+        print(f"Pac-Man може з’їсти точку {direction}.")
+    elif move == 3:
+        print(f"Pac-Man {direction} з’їсть привид.")
+    else:
+        print("Такого в лабіринті не існує.")
+else:
+    print("Такого напрямку не існує.")
