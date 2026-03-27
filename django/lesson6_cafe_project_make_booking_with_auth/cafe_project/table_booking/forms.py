2e2fc19c363f34bec1882fc22fcb04178c4a99a6
olha-tryhub-teacher <olha.tryhub.work@gmail.com>
2026-03-27 23:23:50 +0200

    delete files

--- django/lesson6_cafe_project_make_booking_with_auth/cafe_project/table_booking/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import Booking
-
-
-class BookingForm(forms.ModelForm):
-    class Meta:
-        model = Booking
-        fields = ["client", "table", "date", "start_time", "end_time", "guests_count",]
--- django/task_traking/tasks/templatetags/custom_tags.py
+++
@@ -1,8 +0,0 @@
-from django import template
-
-register = template.Library()
-
-
-@register.filter(name="endswith")
-def endswith(value, arg):
-    return value.lower().endswith(arg.lower())
