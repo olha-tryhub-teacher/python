cd72e8203b52abfa2f82f190d09acce6946eca0a
olha-tryhub-teacher <olha.tryhub.work@gmail.com>
2026-04-04 19:04:48 +0300

    Update code.py

--- code.py
+++ code.py
@@ -1,33 +1,10 @@
-from customtkinter import *
-from random import randint
-
-
-def change_btn_pos():
-   x_random = randint(0, win.winfo_width() - btn_no.winfo_width())
-   y_random = randint(0, win.winfo_height() - btn_no.winfo_height())
-   btn_no.place(x=x_random, y=y_random)
-
-
-def show_win():
-   win2 = CTk()
-   win2.geometry("200x100")
-   win2.title("Мої вітання")
-   label_win = CTkLabel(win2, text="Дуже приємно чути!!!", font=("Arial", 14, "bold"))
-   label_win.pack(pady=20)
-   win2.mainloop()
-
-
-win = CTk()
-win.geometry("400x300")
-win.title("Соціальне опитування")
-
-label = CTkLabel(win, text="Чи подобається тобі навчатися у школі Logika?", font=("Arial", 14, "bold"))
-label.pack(pady=40)
-
-btn_no = CTkButton(win, text="Ні", command=change_btn_pos)
-btn_no.place(x=50, y=200)
-
-btn_yes = CTkButton(win, text="Так", command=show_win)
-btn_yes.place(x=200, y=200)
-
-win.mainloop()
+comment = input("Введіть ваш коментар: ")
+count = 0
+for char in comment:
+    if char == " ":
+        continue
+    count += 1
+if count > max_count:
+    print("Коментар не буде опубліковано: досягнуто ліміт у 125 символів.")
+else:
+    print("Коментар було опубліковано!")
