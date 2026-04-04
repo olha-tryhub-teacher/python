6ea1b09d97b8e0192eedcf68864e4536fc8b1551
olha-tryhub-teacher <olha.tryhub.work@gmail.com>
2026-04-04 19:27:09 +0300

    Update code.py

--- code.py
+++ code.py
@@ -1,8 +1,7 @@
-purchase_limit = 500
-count = 0
-while count <= purchase_limit:
-    price = float(input("Введіть суму покупок?"))
-    count += price
-    print(f"Всього покупок: {count}")
-if count - purchase_limit >= 200:
-    print("Ліміт покупки значно перевищено!")
+while True:
+    password = input("Введіть пароль:")
+    if len(password) <= 8:
+        print("Пароль надто короткий.")
+        continue
+    print("Пароль прийнятий.")
+    break
