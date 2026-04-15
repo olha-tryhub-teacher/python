99c64a486f12cc6c4775b4bc39112284355f86a0
olha-tryhub-teacher <olha.tryhub.work@gmail.com>
2026-04-15 16:30:09 +0300

    Update code.py

--- code.py
+++ code.py
@@ -1,19 +1,45 @@
-print("🎨 Художник малює квіткове поле!")
+# ngrok.exe tcp 8080
+from socket import *
+import threading
 
-# Розміри поля
-rows = int(input("Введыть ширину поля:"))
-cols =  int(input("Введіть довжину поля"))
+server_socket = socket(AF_INET, SOCK_STREAM)
+server_socket.bind(('localhost', 8080))
+server_socket.listen(12)
+print("Сервер запущений...")
 
-# Малюємо поле
-for i in range(rows):
-    line = ""
-    for j in range(cols):
-        if (i + j) % 4 == 0:
-            line += "🌹"
-        elif (i + j) % 4 == 1:
-            line += "🌷"
-        elif (i + j) % 4 == 2:
-            line += "🌼"
-        else:
-            line += "🌸"
-    print(line)
+clients = []
+
+
+def broadcast(message):
+    for client in clients:
+        try:
+            client.send(f"{message}\n".encode())
+        except:
+            pass
+
+
+def handle_client(client_socket):
+    name = client_socket.recv(1024).decode().strip()
+    broadcast(f"{name} приєднався до чату!")
+
+    while True:
+        try:
+            message = client_socket.recv(1024).decode().strip()
+            print(f"{name}: {message}")
+            broadcast(f"{name}: {message}")
+        except:
+            clients.remove(client_socket)
+            broadcast(f"{name} вийшов із чату!")
+            client_socket.close()
+            break
+
+
+while True:
+    client_socket, addr = server_socket.accept()
+    clients.append(client_socket)
+    threading.Thread(target=handle_client, args=(client_socket,), daemon=True).start()
+
+
+
+
+# ngrok.exe tcp номер порта
