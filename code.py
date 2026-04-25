5b1addbeed5aa4317a373ebc659b6ebddd66009c
olha-tryhub-teacher <olha.tryhub.work@gmail.com>
2026-04-25 14:32:50 +0300

    Update code.py

--- code.py
+++ code.py
@@ -1,14 +1,25 @@
-print("Архітектор хоче створити клумбу з трояндами 🌹!")
-height = input("Введіть висоту клумби: ")
-width = input("Введіть ширину клумби: ")
-if height.isdigit() and width.isdigit():
-  height = int(height)
-  width = int(width)
-  print(width*"🧱")
-  for i in range(height-2):
-    print("🧱"+(width-2)*"🌹"+"🧱")
-  print(width*"🧱")
-    
-  print("Клумба готова! Архітектор пишається вашою роботою!")
-else:
-  print("Розміри клумби мають бути числами!")
+import pygame
+from settings import *
+from ui import Button
+
+def main():
+    # Ініціалізація
+    pygame.init()
+    pygame.display.set_caption("Фортепіано")
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+    clock = pygame.time.Clock()
+
+    # Створення клавіш (кнопок)
+    keys = []
+    key_width = WIDTH // 8
+
+    for i in range(7):
+        # Розраховуємо позицію кожної клавіші
+        x_pos = i * key_width + 45
+        # Беремо звук зі списку (якщо звуків менше ніж 8, використовуємо остачу від ділення)
+        sound_path = SOUND_FILES[i]
+
+        btn = Button(
+            x=x_pos, y=100, width=key_width - 2, 
+            height=250, color=WHITE, sound_path=sound_path)
+        keys.append(btn)
