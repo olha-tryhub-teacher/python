93d196482fbccb06ef0944b891dd97d295fb1693
olha-tryhub-teacher <olha.tryhub.work@gmail.com>
2026-05-23 19:11:22 +0300

    Update code.py

--- code.py
+++ code.py
@@ -1,32 +1,24 @@
-from pygame import *
+# підключи свій модуль art
+from turtle import *
 
-init()
+def square(a, col):
+    color(col)
+    for i in range(4):
+        fd(a)
+        lt(90)
 
-size = 1000, 800
-window = display.set_mode(size)
-display.set_caption('Моя гра')
-clock = time.Clock()
 
-player = Rect(200, 200, 50, 50)
-player_speed = 5
+def rectangle(a, b, col):
+    color(col)
+    for i in range(2):
+        fd(a)
+        lt(90)
+        fd(b)
+        lt(90)
 
-while True:
-    for e in event.get():
-       if e.type == QUIT:
-           quit()
 
-    keys = key.get_pressed()
-    if keys[K_w]:
-        player.y -= player_speed
-    if keys[K_s]:
-        player.y += player_speed
-    if keys[K_a]:
-        player.x -= player_speed
-    if keys[K_d]:
-        player.x += player_speed
-
-    window.fill((255, 255, 255))
-    draw.rect(window, (0, 200, 0), player)
-
-    display.update()
-    clock.tick(60)
+def triangle(a, col):
+    color(col)
+    for i in range(3):
+        fd(a)
+        lt(120)
