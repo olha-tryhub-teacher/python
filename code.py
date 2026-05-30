4782fbc7e80e5fe74d77207e31f0a33754abd70f
olha-tryhub-teacher <olha.tryhub.work@gmail.com>
2026-05-30 19:03:14 +0300

    Update code.py

--- code.py
+++ code.py
@@ -1,22 +1,41 @@
-# Напиши функцію christmas_tree
+
+# Напиши функцію high_rise
+from random import randint
 from turtle import *
 from art import *
 
-def christmas_tree(x, y, a, col1, col2):
-    # Малюємо стовбур
-    start(x, y)
-    width(1)
-    square_fill(a, col1)
 
-    # Малюємо гілки (три яруси)
-    x, y = x - a, y + a
+def high_rise2D(x, y, a, b, col1, col2):
+    # Малюємо основу будинку
+    start(x, y)
+    width(4)
+    rectangle_fill(a, b, col1)
+
+    # Розрахунок розмірів та кількості вікон
+    win_size = a // 6
+    n = b // win_size
+    x, y = x + win_size // 2, y + win_size // 2
+    x0 = x
+
+    # Малюємо вікна
     width(1)
-    for i in range(3):
-        start(x, y)
-        # Передаємо col2 як колір хвої
-        triangle_fill(a * 3, col2)
-        y += a
+    for i in range(n // 2):
+        for j in range(3):
+            start(x, y)
+
+            # Визначаємо, чи горить світло
+            if randint(1, 3) == 1:
+                win_color = "darkorange"
+            else:
+                win_color = col2
+
+            # Малюємо вікно з обраним кольором
+            square_fill(win_size, win_color)
+            x += 2 * win_size
+
+        x = x0
+        y += 2 * win_size
+
 
 speed(0)
-# Приклад виклику:
-christmas_tree(0, -100, 30, "brown", "green")
+high_rise2D(0, -150, 80, 140, "grey", "orange")
