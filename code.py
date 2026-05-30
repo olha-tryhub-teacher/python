e6fd9564b968fff111312a200ea5059bfc46bec8
olha-tryhub-teacher <olha.tryhub.work@gmail.com>
2026-05-30 17:57:13 +0300

    Update code.py

--- code.py
+++ code.py
@@ -1,58 +1,12 @@
-from pygame import *
-from math import hypot
+# Малюємо квадрат
+from turtle import *
 
 
-class Ball:
-    def __init__(self, x, y, radius, color, speed):
-        self.speed = speed
-        self.x = x
-        self.y = y
-        self.radius = radius
-        self.color = color
-
-    def move(self):
-        keys = key.get_pressed()
-        if keys[K_UP]:
-            self.y -= self.speed
-        if keys[K_DOWN]:
-            self.y += self.speed
-        if keys[K_LEFT]:
-            self.x -= self.speed
-        if keys[K_RIGHT]:
-            self.x += self.speed
-
-    def reset(self):
-        draw.circle(window, self.color, (self.x, self.y),self.radius)
-
-    def collidecircle(self, ball2):
-        distance = hypot(self.x - ball2.x, self.y - ball2.y)
-        return distance < (self.radius + ball2.radius)
+def square(a, col):
+    color(col)
+    for i in range(4):
+        fd(a)
+        lt(90)
 
 
-init()
-
-size = 500, 500
-
-window = display.set_mode(size)
-display.set_caption("ВАША НАЗВА")
-clock = time.Clock()
-
-bg = image.load("img.png")
-bg = transform.scale(bg, size)
-
-ball = Ball(300, 300, 25, (255, 100, 255), 5)
-
-while True:
-    for e in event.get():
-        if e.type == QUIT:
-            quit()
-
-    window.blit(bg, (0, 0))
-    ball.move()
-    ball.reset()
-
-    display.update()
-    clock.tick(60)
-
-
-
+square(50, "red")
