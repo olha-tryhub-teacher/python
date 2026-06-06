b7ae04ea88c6e7992c32efed1f5b0698712c7ca1
olha-tryhub-teacher <olha.tryhub.work@gmail.com>
2026-06-06 17:19:13 +0300

    Update code.py

--- code.py
+++ code.py
@@ -1,34 +1,18 @@
-        for conn in list(players.keys()):
-            if conn in eliminated:
-                try:
-                    conn.send("LOSE".encode())
-                except:
-                    pass
-                to_remove.append(conn)
-                continue
-
-            try:
-                packet = "|".join([f"{p['id']},{p['x']},{p['y']},{p['r']},{p['name']}" for c, p in players.items() if c != conn and c not in eliminated]) + "|"
-                conn.send(packet.encode())
-            except:
-                to_remove.append(conn)
-
-        for conn in to_remove:
-            players.pop(conn, None)
-            conn_ids.pop(conn, None)
-
-
-Thread(target=handle_data, daemon=True).start()
-print("SERVER running...")
-
-while True:
-    try:
-        conn, addr = sock.accept()
-        conn.setblocking(False)
-        id_counter += 1
-        players[conn] = {"id": id_counter, "x": 0, "y": 0, "r": 20, "name": None}
-        conn_ids[conn] = id_counter
-        conn.send(f"{id_counter},0,0,20".encode())
-    except:
-        pass
-
+def receive_data():
+    global all_players, lose
+    while True:
+        try:
+            data = sock.recv(4096).decode()
+            if "LOSE" in data:
+                lose = True
+            elif data:
+                # Оновлюємо дані ворогів
+                for p in data.split("|"):
+                    if "," in p:
+                        parts = p.split(",")
+                        if len(parts) >= 4:
+                            pid = int(parts[0])
+                            if pid != my_id:
+                                all_players[pid] = [int(parts[1]), int(parts[2]), int(parts[3])]
+        except:
+            break
