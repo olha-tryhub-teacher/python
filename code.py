78c960c690df5a93e5ca306689036c88979f6ee6
olha-tryhub-teacher <olha.tryhub.work@gmail.com>
2026-06-06 16:51:50 +0300

    Update code.py

--- code.py
+++ code.py
@@ -8,8 +8,7 @@
                 continue
 
             try:
-                packet = "|".join([f"{p["id"]},{p["x"]},{p["y"]},{p["r"]},{p["name"]}"
-                                   for c, p in players.items() if c != conn and c not in eliminated]) + "|"
+                packet = "|".join([f"{p["id"]},{p["x"]},{p["y"]},{p["r"]},{p["name"]}" for c, p in players.items() if c != conn and c not in eliminated]) + "|"
                 conn.send(packet.encode())
             except:
                 to_remove.append(conn)
