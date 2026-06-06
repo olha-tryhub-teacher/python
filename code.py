969c62a523e1a9f0503ab40a9b4269991a6a681b
olha-tryhub-teacher <olha.tryhub.work@gmail.com>
2026-06-06 19:20:51 +0300

    Update code.py

--- code.py
+++ code.py
@@ -1,1 +1,20 @@
-    print(f"x: {new_coordinates[i]} ; y: {new_coordinates[i+1]}")
+valid_chars = "0123456789ABCDEFabcdef"
+pallet = list()
+#функція перевірки кольору
+def chek_color(color):
+  if color[0] == "#" and len(color) == 7:
+    for c in color[1:]:
+      if c not in valid_chars:
+        return False 
+  else:
+    return False
+  
+  return True
+ans = input("Color ?")  
+while ans != "0":
+  if chek_color(ans):
+    pallet.append(ans)
+  else:
+    print("Некоректно введений колір.")
+  ans = input("Color ?")
+print(pallet)
