f99ff72aa678b53baf9427ba49e340b3d7d06599
olha-tryhub-teacher <olha.tryhub.work@gmail.com>
2026-06-13 18:56:11 +0300

    Update code.py

--- code.py
+++ code.py
@@ -2,17 +2,12 @@ from turtle import *
 from art import *
 
 speed(0)
-ht()
+hideturtle()
 
 
-def cross(x, y, size, col):
+def zero(x, y, size, col):
+    start(x + size // 2, y)
+    width(5)
+    setheading(0)
     color(col)
-    start(x, y)
-    width(4)
-    setheading(45)
-    forward(1.4 * size)
-    start(x + size, y)
-    setheading(135)
-    forward(1.4 * size)
-
-cross(150, 0, 120, "green")
+    circle(size // 2)
