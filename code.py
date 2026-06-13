a04e6d1e6b5df122b95b1cef05d8c06d6a2b62d5
olha-tryhub-teacher <olha.tryhub.work@gmail.com>
2026-06-13 19:02:29 +0300

    Update code.py

--- code.py
+++ code.py
@@ -1,13 +1,21 @@
 from turtle import *
-from art import *
 
-speed(0)
-hideturtle()
+playingField = [-1, -1, -1, -1, -1, -1, -1, -1, -1]
 
+def move_player(player, draw_function):
+    if player == 0:
+        print("Ходить нулик.Введіть номер клiтинки:")
+        c = "#9b59b6"
+    else:
+        print("Ходить хрестик.Введіть номер клiтинки:")
+        c = "#e74c3c"
+    cell = int(input())
 
-def zero(x, y, size, col):
-    start(x + size // 2, y)
-    width(5)
-    setheading(0)
-    color(col)
-    circle(size // 2)
+    while playingField[cell - 1] != -1:
+        print("Ця клітинка вже зайняти. Оберіть іншу!")
+        cell = int(input())
+    cell = cell - 1
+    i, j = cell % 3, cell // 3
+    x, y = xCor[i], yCor[j]
+    draw_function(x, y, size, c)
+    playingField[cell] = player
