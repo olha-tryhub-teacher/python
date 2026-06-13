86ad10f9e450e499d3631d2c45074cefc60de58c
olha-tryhub-teacher <olha.tryhub.work@gmail.com>
2026-06-13 19:22:20 +0300

    Update code.py

--- code.py
+++ code.py
@@ -1,3 +1,46 @@
+def field(col):
+    xStart,yStart = -150,50
+    size = 100
+    x,y = xStart, yStart
+    for i in range(3):
+        for j in range(3):
+            start(x,y)
+            square(size,10,col)
+            x += size
+        x = xStart
+        y -= size
+       
+def draw_cross(x,y,col):
+    size = 100
+    start(x,y)
+    color(col)
+    width(10)
+    setheading(45)
+    fd(1.4*size)
+    start(x+size, y)
+    setheading(135)
+    fd(1.4*size)
+def draw_dot(x,y,col):
+    start( x+size//2, y)
+    setheading(0)
+    color(col)
+    circle( size//2)
+   
+def move_player(player, col):
+    cell = int(input("Введіть номер клітинки від 1 до 9"))
+    while playing_field[cell]!=-1:
+        print("Клітинки вже зайнята!")
+        cell = int(input("Введіть номер клітинки від 1 до 9"))
+    x = x_cor[cell]
+    y = y_cor[cell]
+    start(x,y)
+    playing_field[cell] = player
+    color(col)
+    if player == 1:
+        draw_cross(x,y,col)
+    else:
+        draw_dot(x,y,col)
+       
 def check_win():
     # 1 = 2 = 3
     if playing_field[1] == playing_field[2] == playing_field[3] :
@@ -24,4 +67,7 @@ def check_win():
     if playing_field[3] == playing_field[5] == playing_field[7] :
         return playing_field[3]
 
+
     return -1  # Ніхто не виграв
+
+
