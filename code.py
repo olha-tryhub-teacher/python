f5db8f94dd01688dcbc190367d09b5ac65a4b2e2
olha-tryhub-teacher <olha.tryhub.work@gmail.com>
2026-06-13 15:54:23 +0300

    Update code.py

--- code.py
+++ code.py
@@ -1,84 +1,90 @@
+from math import hypot
+from pygame import *
+from random import randint
 from socket import socket, AF_INET, SOCK_STREAM
 from threading import Thread
-import time
 
+# Налаштування мережі
+HOST = "127.0.0.1"
+PORT = 5000
 sock = socket(AF_INET, SOCK_STREAM)
-sock.bind(("0.0.0.0", 5000))
-sock.listen(15)
-sock.setblocking(False)
+sock.connect((HOST, PORT))
 
-players = {}
-conn_ids = {}
-id_counter = 0
+# Отримуємо свої дані при старті
+resv_data = sock.recv(64).decode().split(",")
+my_id, my_x, my_y, my_r = map(int, resv_data)
+
+# Налаштування вікна
+size = (1000, 800)
+init()
+window = display.set_mode(size)
+clock = time.Clock()
+
+all_players = {}  # Словник {id: [x, y, r]}
 
 
-def handle_data():
-    global id_counter
+def receive_data():
+    global all_players, lose
     while True:
-        time.sleep(0.01)
-        player_data = {}
-        to_remove = []
-
-        for conn in list(players):
-            try:
-                data = conn.recv(64).decode().strip()
-                if "," in data:
-                    parts = data.split(",")
-                    if len(parts) == 5:
-                        pid, x, y, r = map(int, parts[:4])
-                        name = parts[-1]
-                        players[conn] = {"id": pid, "x": x,
-                                         "y": y, "r": r,
-                                         "name": name}
-                        player_data[conn] = players[conn]
-            except:
-                continue
-
-        eliminated = []
-        for conn1 in player_data:
-            if conn1 in eliminated: continue
-            p1 = player_data[conn1]
-            for conn2 in player_data:
-                if conn1 == conn2 or conn2 in eliminated: continue
-                p2 = player_data[conn2]
-                dx, dy = p1["x"] - p2["x"], p1["y"] - p2["y"]
-                distance = (dx ** 2 + dy ** 2) ** 0.5
-                if (distance < p1["r"] + p2["r"]
-                        and p1["r"] > p2["r"] * 1.1):
-                    p1["r"] += int(p2["r"] * 0.5)
-                    players[conn1] = p1
-                    eliminated.append(conn2)
-
-        for conn in list(players.keys()):
-            if conn in eliminated:
-                try:
-                    conn.send("LOSE".encode())
-                except:
-                    pass
-                to_remove.append(conn)
-                continue
-
-            try:
-                packet = "|".join([f"{p['id']},{p['x']},{p['y']},{p['r']},{p['name']}" for c, p in players.items() if c != conn and c not in eliminated]) + "|"
-                conn.send(packet.encode())
-            except:
-                to_remove.append(conn)
-
-        for conn in to_remove:
-            players.pop(conn, None)
-            conn_ids.pop(conn, None)
+        try:
+            data = sock.recv(4096).decode()
+            if "LOSE" in data:
+                lose = True
+            elif data:
+                # Оновлюємо дані ворогів
+                for p in data.split("|"):
+                    if "," in p:
+                        parts = p.split(",")
+                        if len(parts) >= 4:
+                            pid = int(parts[0])
+                            if pid != my_id:
+                                all_players[pid] = [int(parts[1]), int(parts[2]), int(parts[3])]
+        except:
+            break
 
 
-Thread(target=handle_data, daemon=True).start()
-print("SERVER running...")
+Thread(target=receive_data, daemon=True).start()
 
-while True:
-    try:
-        conn, addr = sock.accept()
-        conn.setblocking(False)
-        id_counter += 1
-        players[conn] = {"id": id_counter, "x": 0, "y": 0, "r": 20, "name": None}
-        conn_ids[conn] = id_counter
-        conn.send(f"{id_counter},0,0,20".encode())
-    except:
-        pass
+
+class Ball:
+    def __init__(self, x, y, radius, color, speed=0):
+        self.x, self.y = x, y
+        self.radius = radius
+        self.color = color
+        self.base_speed = speed
+        self.scale = 1.0
+        self.growth_limit = 60
+
+    def update_player(self, cells):
+        # Масштабування
+        self.scale = self.growth_limit / self.radius if self.radius > self.growth_limit else 1.0
+
+        keys = key.get_pressed()
+        speed = 15 * self.scale
+        if keys[K_UP]: self.y -= speed
+        if keys[K_DOWN]: self.y += speed
+        if keys[K_LEFT]: self.x -= speed
+        if keys[K_RIGHT]: self.x += speed
+
+        # Логіка поїдання (спрощено)
+        for cell in cells[:]:
+            if self.collidecircle(cell):
+                self.radius += cell.radius * 0.2
+                cells.remove(cell)
+
+    def draw(self, surface, camera_x, camera_y, camera_scale):
+        sx = int((self.x - camera_x) * camera_scale + size[0] // 2)
+        sy = int((self.y - camera_y) * camera_scale + size[1] // 2)
+        r = int(self.radius * camera_scale)
+        draw.circle(surface, self.color, (sx, sy), max(2, r))
+
+    def collidecircle(self, ball2):
+        distance = hypot(self.x - ball2.x, self.y - ball2.y)
+        return distance < (self.radius + ball2.radius)
+
+# TODO: МИ ТУТ ЗУПИНИЛИСЯ!!!!
+# Ініціалізація
+ball = Ball(my_x, my_y, my_r, (0, 255, 100), speed=15)
+cells = [Ball(randint(-2000, 2000), randint(-2000, 2000), 10, (200, 200, 0)) for _ in range(300)]
+f = font.Font(None, 50)
+running, lose = True, False
