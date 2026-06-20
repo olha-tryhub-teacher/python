689f0e7e92eb8642ca944dc1c36886a2140d7a75
olha-tryhub-teacher <olha.tryhub.work@gmail.com>
2026-06-20 19:01:07 +0300

    Update code.py

--- code.py
+++ code.py
@@ -1,11 +1,14 @@
-# Функція для малювання плосі для правильних літер
-def write_ask(word):
-    start(-170, 100)
-    setheading(0)
-    width(4)
+def write_right(letter, word):
+    start(-170, 105)
+    penup()
     color("black")
+    setheading(0)
+    count = 0
     for w in word:
-        fd(30)
-        penup()
-        fd(15)
-        pendown()
+        if w == letter:
+            pendown()
+            write(letter, font=("Arial", 32))
+            penup()
+            count += 1
+        fd(45)
+    return count
