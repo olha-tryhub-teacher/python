9e8a016ef79874be8925ac280b5cc6276a390252
olha-tryhub-teacher <olha.tryhub.work@gmail.com>
2026-08-01 18:16:36 +0300

    Update code.py

--- code.py
+++ code.py
@@ -1,12 +1,2 @@
-# побудувати трасу, де є старт та фініш
-def track():
-    tr = create_turtle(-150, -150, "triangle", "green")
-    tr.setheading(90)
-    tr.pendown()
-    tr.forward(300)
-    tr.color("red")
-    tr.width(20)
-    tr.penup()
-    tr.goto(300, -150)
-    tr.pendown()
-    tr.forward(300)
+def win(t):
+    t.write("i am a winner!")
