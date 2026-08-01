05f4328a602625c6ec6cdc7583fc3874a6692070
olha-tryhub-teacher <olha.tryhub.work@gmail.com>
2026-08-01 19:27:09 +0300

    Update code.py

--- code.py
+++ code.py
@@ -1,16 +1,73 @@
-# створити черепашок
-t1 = create_turtle(-150, 70, "turtle", "purple")
-t2 = create_turtle(-150, -70, "turtle", "orange")
+# ваш код
+from turtle import *
+from random import randint
 
-track()
-
-# створити ігровий цикл, де черепашки будуть змагатись
-while t1.xcor() < 300 and t2.xcor() < 300:
-    t1.forward(randint(1, 7)) # 7
-    t2.forward(randint(1, 7)) # 1
+hideturtle()
 
 
-if t1.xcor() > t2.xcor(): #305, 301
-    win(t1)
-else:
-    win(t2)
+def create_t(x, y, h, col):
+    t = Turtle()
+    t.color(col)
+    t.shape("turtle")
+    t.setheading(h)
+    t.penup()
+    t.goto(x, y)
+    t.pendown()
+    return t
+
+
+# 1.Ігрове поле
+screen = getscreen()
+screen.bgcolor("yellow")
+
+# 2.Лейбли-лічильник
+click = create_t(200, 100, 0, "blue")
+click.count = 0
+click.hideturtle()
+click.write(f"Click: {click.count}", font=("Arial", 24))
+
+# те саме тільки раннер
+runner = create_t(200, 150, 0, "red")
+runner.count = 0
+runner.hideturtle()
+runner.write(f"Runner: {runner.count}", font=("Arial", 24))
+
+# 3.Черепашки-втікачки
+# 3.1 створити черепашок
+t1 = create_t(0, 0, 0, "pink")
+
+
+# 3.2 функції-обробники кліку
+def click1(x, y):
+    t1.lt(randint(30, 200))
+    click.count += 1
+    click.clear()
+    click.write(f"Click: {click.count}", font=("Arial", 24))
+
+
+# 3.3 підписка на подію клік по черепашках
+t1.onclick(click1)
+
+# 4. Ігровий цикл
+while click.count < 20 and runner.count < 4:
+    if abs(t1.xcor()) < 200 and abs(t1.ycor()) < 200:
+        t1.forward(2)
+    else:
+        runner.count += 1
+        runner.clear()
+        runner.write(f"Runner: {runner.count}", font=("Arial", 24))
+
+
+# 5. Переврка виграшу/програшу
+if click.count >= 20:
+    click.penup()
+    click.goto(0, 0)
+    click.write("You winn", font=("Arial", 40))
+# те саме тільки раннер
+if runner.count >= 4:
+    runner.penup()
+    runner.goto(0, 0)
+    runner.write("You lose", font=("Arial", 40))
+
+
+done()
