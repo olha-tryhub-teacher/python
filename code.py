6fa68731b365800e891926adc356b4ab895931b8
olha-tryhub-teacher <olha.tryhub.work@gmail.com>
2026-08-22 14:44:36 +0300

    Update code.py

--- code.py
+++ code.py
@@ -1,32 +1,18 @@
+    # КЛІК ПО РИБІ
+    def on_touch_down(self, touch):
+        # Якщо клік був не по рибі або вона прозора — передаємо клік далі
+        if not self.collide_point(*touch.pos) or not self.opacity:
+            return super().on_touch_down(touch)
 
-# 4. Клас кнопки дії (успадковує базову кнопку)
-class ActionButton(Button):
-    def __init__(self, x, y, col, name, action_func):
-        # Викликаємо ініціалізацію базової кнопки
-        super().__init__(x, y, "square", col, name)
+        self.hp_current -= 1
+        self.GAME_SCREEN.score += 1
 
-        self.action_func = action_func
-        self.onclick(self.do_action)
+        if self.hp_current <= 0:
+            self.defeated()
+            Clock.schedule_once(self.GAME_SCREEN.level_complete, 1.2)
+        else:
+            self.stop_timers()
+            self.hide_fish()
 
-    def do_action(self, x, y):
-        self.action_func()
-
-
-# === ОСНОВНА ПРОГРАМА ===
-
-pen = Pen()
-
-sidebar_x = -250  # Координата X для всієї бічної панелі (зліва)
-
-# Створюємо кнопки кольорів
-ColorButton(sidebar_x, 200, "black", "Чорний", pen)
-ColorButton(sidebar_x, 160, "red", "Червоний", pen)
-ColorButton(sidebar_x, 120, "blue", "Синій", pen)
-ColorButton(sidebar_x, 80, "green", "Зелений", pen)
-ColorButton(sidebar_x, 40, "orange", "Помаранчевий", pen)
-# Створюємо кнопки дій
-ActionButton(sidebar_x, 0, "darkgrey", "Товстіше", pen.increase_width)
-ActionButton(sidebar_x, -40, "lightgrey", "Тонше", pen.decrease_width)
-ActionButton(sidebar_x, -80, "red", "Очистити", pen.clear)
-
-done()
+        # Повертаємо True, щоб система знала: "Клік був влучним, ми його обробили!"
+        return True
