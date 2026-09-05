2ef0d2abb05a0aed9ec5a8c64c60deb559f24a53
olha-tryhub-teacher <olha.tryhub.work@gmail.com>
2026-09-05 14:30:53 +0300

    Update code.py

--- code.py
+++ code.py
@@ -1,10 +1,9 @@
-# Загальний план будинку (список усіх кімнат)
-plan_house = [
-    data_room1,
-    data_room2,
-    data_living,
-    data_corridor,
-    data_toilet,
-    data_bath,
-    data_kitchen
-]
+# ДОДАНО: кольори двох тем. Їх можна змінити тут, не редагуючи Python-код.
+#:set light_background "#FFF5F5"
+#:set light_text "#4A4A4A"
+#:set light_button "#F7D6D0"
+#:set light_button_pressed "#E2B4BD"
+#:set dark_background "#092328"
+#:set dark_text "#8BBB92"
+#:set dark_button "#12544F"
+#:set dark_button_pressed "#2A835F"
