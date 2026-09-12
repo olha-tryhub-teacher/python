8f107b8960301bcb0e5d3ec7f828789c25e44dbe
olha-tryhub-teacher <olha.tryhub.work@gmail.com>
2026-09-12 18:13:41 +0300

    Update code.py

--- code.py
+++ code.py
@@ -1,21 +1,43 @@
-# --- Базовий клас для всіх спрайтів ---
-class Sprite(Turtle):
-    def __init__(self, x, y, col, sh):
-        super().__init__()
-        # t = Turtle()
-        self.color(col)
-        self.shape(sh)
-        self.go_to(x, y)
+# --- Клас гравця (керується з клавіатури) ---
+class Player(Sprite):
+    def __init__(self, x, y, col, sh, step_size):
+        super().__init__(x, y, col, sh)
+        self.step_size = step_size
 
 
-    # Переміщення спрайта без малювання
-    def go_to(self, x, y):
-        self.penup()
-        self.goto(x, y)
+        # Прив'язка клавіш до функцій руху
+        screen.onkey(self.move_left, "Left")
+        screen.onkey(self.move_right, "Right")
+        screen.onkey(self.move_down, "Down")
+        screen.onkey(self.move_up, "Up")
+        screen.listen()
 
 
-    # Перевірка зіткнення з іншим об’єктом
-    def touch_t(self, t):
-        if abs(self.xcor() - t.xcor()) < 20 and abs(self.ycor() - t.ycor()) < 20:
-            return True
-        return False
+    # Рух вліво
+    def move_left(self):
+        self.setheading(180)
+        self.forward(self.step_size)
+
+
+    # Рух вправо
+    def move_right(self):
+        self.setheading(0)
+        self.forward(self.step_size)
+
+
+    # Рух вгору
+    def move_up(self):
+        self.setheading(90)
+        self.forward(self.step_size)
+
+
+    # Рух вниз
+    def move_down(self):
+        self.setheading(270)
+        self.forward(self.step_size)
+
+
+    # Виведення повідомлення про завершення гри
+    def write_end(self, txt):
+        self.go_to(-150, 0)
+        self.write(txt, font=("Arial", 30))
