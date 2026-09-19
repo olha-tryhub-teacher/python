a59e4233043974ea7edba1b94f80493201f5fd31
olha-tryhub-teacher <olha.tryhub.work@gmail.com>
2026-09-19 18:31:44 +0300

    Update code.py

--- code.py
+++ code.py
@@ -1,15 +1,51 @@
-        if event.type == pygame.MOUSEBUTTONDOWN and (
-                (event.pos[0] - self.circle_x) ** 2 + (
-                event.pos[1] - self.rect.centery) ** 2
-        ) ** 0.5 < 15:
-            self.dragging = True
-        elif event.type == pygame.MOUSEBUTTONUP:
-            self.dragging = False
+# ваш код
+from turtle import *
+from random import randint
 
-        if self.dragging and event.type == pygame.MOUSEMOTION:
-            # Обмежуємо рух повзунка межами слайдера
-            self.circle_x = max(self.rect.left,
-                                min(event.pos[0], self.rect.right))
-            self.val = (self.circle_x - self.rect.x) / self.rect.width
-            return True  # Повертаємо True, щоб знати, що гучність змінилася
-        return False
+
+def create_turtle(x, y, sh, col):
+    t = Turtle()
+    t.pu()
+    t.goto(x, y)
+    t.width(10)
+    t.shape(sh)
+    t.color(col)
+    return t
+
+
+# побудувати трасу, де є старт та фініш
+def track():
+    tr = create_turtle(-150, -150, "triangle", "green")
+    tr.setheading(90)
+    tr.pendown()
+    tr.forward(300)
+    tr.color("red")
+    tr.width(20)
+    tr.penup()
+    tr.goto(300, -150)
+    tr.pendown()
+    tr.forward(300)
+
+
+def win(t):
+    t.write("i am a winner!")
+
+
+# створити черепашок
+t1 = create_turtle(-150, 70, "turtle", "purple")
+t2 = create_turtle(-150, -70, "turtle", "orange")
+
+track()
+
+# створити ігровий цикл, де черепашки будуть змагатись
+while t1.xcor() < 300 and t2.xcor() < 300:
+    t1.forward(randint(1, 7)) # 7
+    t2.forward(randint(1, 7)) # 1
+
+
+if t1.xcor() > t2.xcor(): #305, 301
+    win(t1)
+else:
+    win(t2)
+
+done()
